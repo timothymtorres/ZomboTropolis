3bbc9c5d6675ed441113ef67250c280a24a7fab5
Timothy <timothymtorres@gmail.com>
2016-09-19 16:43:39 -0500

    Updated dice methods to new rl-dice version

--- code/player/armor/class.lua
+++ code/player/armor/class.lua
@@ -7,7 +7,7 @@ function armor:initialize(player)
   self.player = player
 end
 
-function armor:passDurabilityCheck(degrade_chance) return dice:rollAndTotal(self.durability) > degrade_chance end
+function armor:passDurabilityCheck(degrade_chance) return dice:roll(self.durability) > degrade_chance end
 
 function armor:getProtection(damage_type) return self.protection[damage_type] or 0 end
 
