6a1882d5281449e6f1d77b706484b38eac7e5d55
Timothy <timothymtorres@gmail.com>
2017-08-12 22:30:14 -0500

    Make armor items one use, and relocate durability values to a different file
    Item list now has armor durability to 0, and the prior values are
    transferred to code/player/armor/item_list.lua

--- code/item/list/armor.lua
+++ code/item/list/armor.lua
@@ -2,32 +2,15 @@ local armor = {}
 
 --[[
   full_name = 'insert name'
-  durability = num  (average # of attacks it takes to wear armor out) 
-  resistance = {
-    condition = {protection=num},
-  }  
-  
-  ** As condition becomes worse armor starts to lose resistance (with the exception of a few armors)
+  durability = 0 (all armor is one_use) 
 --]]
 
 armor.leather = {}
 armor.leather.full_name = 'leather jacket'
-armor.leather.durability = 32
-armor.leather.resistance = {
-  [1] = {blunt=1},
-  [2] = {blunt=1},
-  [3] = {blunt=1},
-  [4] = {blunt=1},  
-}
+armor.leather.durability = 0
 
 armor.firesuit = {}
 armor.firesuit.full_name = 'firesuit'
-armor.firesuit.durability = 4
-armor.firesuit.resistance = {
-  [1] = {acid=1},
-  [2] = {acid=2},
-  [3] = {acid=3},
-  [4] = {acid=4},
-}
+armor.firesuit.durability = 0
 
 return armor
