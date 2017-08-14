22686296a5e70a57b2b16f12da7407b00b945c60
Timothy <timothymtorres@gmail.com>
2017-08-13 20:50:27 -0500

    Refactored syringe.lua to use widget buttons and be similiar to acid.lua format

--- scenes/action/syringe.lua
+++ scenes/action/syringe.lua
@@ -16,48 +16,34 @@ local widget = require('widget')
 
 -- local forward references should go here
 
+-- local forward references should go here
+local cancel_button = require('scenes.action.button.cancel_button')
+local perform_button = require('scenes.action.button.perform_button')
+local target_picker_wheel = require('scenes.action.button.target_picker_wheel')
+
 -- 52 is default tabbar height
-local width, height = display.contentWidth, display.contentHeight - 52
+local width, height = display.contentWidth, display.contentHeight - 52 --320, 428
 
-local container_w, container_h = 280, 150
-local container_xtra_w, container_xtra_h = 0, 0
-local container_xtra 
+local top_container_w, top_container_h = math.floor(width*0.875 + 0.5), math.floor(height*0.35 + 0.5)
+local bottom_container_w, bottom_container_h = math.floor(width*0.925 + 0.5), math.floor(height*0.518 + 0.5)
 
-local extra_widget_sizes = {width=296, height=222}
-
-local button_w, button_h, divider = 110, 40, 15
+local divider = 15
 local action_text
 local listener = {}
 
-local cancelButtonEvent = function(event)
-  if ('ended' == event.phase) then
-    print('Button was pressed and released')
-    if active_timer then timer.cancel(active_timer) end    
-    composer.hideOverlay('fade', 400)
-  end
-end
+local wheel, targets
+local action_params = {}
 
-local targets, wheel
+-- dunno about these--
+------------------
+--------------------
 local item, inv_id 
 
-local function getActionText(action)
-  local str
-  
-  -- need to get syringe stats (condition, accuracy, etc.)
-  -- need to make sure correct params are being passed to action text (ie. the wheel values)
-  -- syringe is not weapon, convert into item and get condition
-  
-  
+local function getActionText()
   local selections = wheel:getValues()  -- {selections[i].value, selections[i].index} [1]=targets, [2]=weapons
-  local target_name = selections[1].value
-  --local weapon_name, target_name = selections[2].value, selections[1].value    
+  local target_name = selections[1].value  
   local condition = '{'..item:getCondition()..'}' or ''       
-  
-  str = 'Inject '..target_name..' ('..targets[selections[1].index]:getStat('hp')..'hp) using syringe?'
-
-  --\n('..item:getToHit(main_player, target)..'% to-hit)   '..condition        
-
-  return str
+  return 'Inject '..target_name..' ('..targets[selections[1].index]:getStat('hp')..'hp) using syringe?'  
 end
 
 local function getActionParams(action)
@@ -65,33 +51,22 @@ local function getActionParams(action)
   local selections = wheel:getValues()
   local target = targets[selections[1].index]
   params = {inv_id, target}  
---[[
-  elseif action == 'barricade' then
-    local inventory_ID = action_params.inv_id
-    params = {inventory_ID}
-  end
---]]
   return params
 end
 
-local function getWheel()
-  targets = main_player:getTargets()
-  local target_names = {}
-  
-  for i in ipairs(targets) do
-    local target_class = targets[i]:getClassName()
-    if target_class == 'player' then
-      target_names[#target_names+1] = targets[i]:getUsername()
-    end
+local performButtonEvent = function(event_button)
+  if ('ended' == event_button.phase) then
+    print('Perform button was pressed and released')    
+    if active_timer then timer.cancel(active_timer) end
+    
+    local selections = wheel:getValues()   
+    action_params[#action_params + 1] = inv_id     
+    action_params[#action_params + 1] = targets[selections[1].index] --target   
+    
+    main_player:takeAction(unpack(action_params))
+    composer.hideOverlay('fade', 400)       
+    composer.gotoScene('scenes.action')
   end
-  
-  local columnData = {
-    {align='center', startIndex=1, labels=target_names},
-  }
-
-  local pick_wheel = widget.newPickerWheel{top=-1*(container_xtra_h*0.5), left=-1*(container_xtra_w*0.5)-15, columns=columnData, columnColor={0.2,0.2,0.2,1}}
-  
-  return pick_wheel  
 end
 
 ---------------------------------------------------------------------------------
@@ -105,96 +80,68 @@ function scene:create( event )
    inv_id = event.params.inv_id
    item = main_player.inventory:lookup(event.params.inv_id)
    
-   container_xtra_w = extra_widget_sizes and extra_widget_sizes.width or 0
-   container_xtra_h = extra_widget_sizes and extra_widget_sizes.height or 0
+   action_params[#action_params + 1] = action   
    
    -- Initialize the scene here.
    -- Example: add display objects to "sceneGroup", add touch listeners, etc.
     
-    local bar_h = 30        
-    local container = display.newContainer( container_w, container_h + bar_h)
-    -- TAB BAR HEIGHT = 60  (so 480-60 /2)
-    container:translate( width*0.5, height*0.5 + bar_h*0.5 - container_xtra_h*0.5) -- center the container
-
-    container_h = container_h - bar_h*0.5
-
-    container_h = container_h
-    local background = display.newRect(0, 0, container_w, container_h)
-    background:setFillColor(0.1, 0.1, 0.1, 0.70)
-    container:insert(background)
+    -------------------------------
+    -------------------------------
+    -- T O P   C O N T A I N E R --
+    -------------------------------
+    -------------------------------
     
-    local top_background_bar = display.newRect(0, -1*(container_h/2 + bar_h*0.5), container_w, bar_h) 
+    local bar_h = 30        
+    local top_container = display.newContainer( top_container_w, top_container_h + bar_h)  
+    top_container:translate( width*0.5, height*0.5 + bar_h*0.5 - bottom_container_h*0.5) -- center the container
+    top_container_h = top_container_h - bar_h*0.5  -- center the container along the y-axis?!?
+
+    local background = display.newRect(0, 0, top_container_w, top_container_h)
+    background:setFillColor(0.1, 0.1, 0.1, 0.70)
+    top_container:insert(background)
+    
+    local top_background_bar = display.newRect(0, -1*(top_container_h/2 + bar_h*0.5), top_container_w, bar_h) 
     top_background_bar:setFillColor(0.2, 0.2, 0.8, 0.70)
-    container:insert(top_background_bar)
+    top_container:insert(top_background_bar)
     
     local action_cost = display.newText{
       text = 'Perform Action For: '..params.cost..' AP', 
       x = 0,
-      y = -1*(container_h/2 + bar_h*0.375), 
+      y = -1*(top_container_h/2 + bar_h*0.375), 
       font = native.systemFont, 
       fontSize = 14,
     }
     action_cost:setFillColor(1, 0, 0, 1)
-    container:insert(action_cost)
+    top_container:insert(action_cost)
     
-    local performButtonEvent = function(event_button)
-      if ('ended' == event_button.phase) then
-        if active_timer then timer.cancel(active_timer) end
-        
-        print('')
-        local test = {unpack(getActionParams(action))}
---for k,v in pairs(event) do print(k,v) end
---for k,v in pairs(event.params) do print(k,v) end
-        print('getting action params BEFORE test:')
-        for i,v in ipairs(getActionParams(test)) do print(i,v) end
-        
+    --------------------
+    -- PERFORM BUTTON --
+    --------------------
+    perform_button.top, perform_button.left = top_container_h/7, -1*(perform_button.width + divider)   
+    perform_button.onEvent = performButtonEvent
+    perform_button = widget.newButton(perform_button)
+    top_container:insert(perform_button) 
 
-        print('inv_id for params is:', event.params.inv_id)
-        test[#test+1] = event.params.inv_id
-        print('getting action params AFTER test:')
-        for i,v in ipairs(getActionParams(test)) do print(i,v) end
-        
-        main_player:takeAction(action, unpack(getActionParams(test)))
-        composer.hideOverlay('fade', 400)       
-        composer.gotoScene('scenes.action')
-        print('Button was pressed and released')
-      end
-    end
+    -------------------
+    -- CANCEL BUTTON --
+    -------------------
+    cancel_button.top, cancel_button.left = top_container_h/7, divider    
+    cancel_button = widget.newButton(cancel_button) 
+    top_container:insert(cancel_button) 
 
-    local perform_button = widget.newButton
-      {
-          left = -1*(button_w + divider),
-          top = container_h/7,
-      --  id = ,
-          label = 'PERFORM',
-          onEvent = performButtonEvent,
-          shape = 'rect',
-          width = button_w,
-          height = button_h,
-          fillColor = { default={ 1, 0, 0, 1 }, over={ 1, 0.1, 0.7, 0.4 } },
-          strokeColor = { default={ 1, 0.4, 0, 1 }, over={ 0.8, 0.8, 1, 1 } },
-          strokeWidth = 4      
-      }  
-    container:insert(perform_button) -- insert and center text
+    -------------------------------------
+    -------------------------------------
+    -- B O T T O M   C O N T A I N E R --
+    -------------------------------------
+    -------------------------------------
 
-    local cancel_button = widget.newButton
-      {
-          left = divider,
-          top = container_h/7,
-      --  id = ,
-          label = 'CANCEL',
-          onEvent = cancelButtonEvent,
-          shape = 'rect',
-          width = button_w,
-          height = button_h,
-          fillColor = { default={ 1, 0, 0, 1 }, over={ 1, 0.1, 0.7, 0.4 } },
-          strokeColor = { default={ 1, 0.4, 0, 1 }, over={ 0.8, 0.8, 1, 1 } },
-          strokeWidth = 4      
-      }  
-    container:insert(cancel_button) -- insert and center text   
-
-    container_xtra = display.newContainer(container_xtra_w, container_xtra_h)
-    container_xtra:translate( width*0.5, height - (container_h)) -- center the container   
+    local bottom_container = display.newContainer(bottom_container_w, bottom_container_h)
+    bottom_container:translate( width*0.5, height - (top_container_h)) -- center the container   
+    
+    -----------------------------------------------------------------------------------------------------------
+    -- These functions are so that the wheel values update the action text in real time while it is spinning --
+    ---------- Possibly change or remove these later when the sprites are added and wheel is removed ----------
+    -----------------------------------------------------------------------------------------------------------    
     
     local function redoActionText()
         action_text:removeSelf()
@@ -202,7 +149,7 @@ function scene:create( event )
         
         action_text = display.newText{
           text = getActionText(action),
-          width = container_w - 10, 
+          width = top_container_w - 10, 
           x = 5,
           y = -40,
           font = native.systemFont,
@@ -210,10 +157,8 @@ function scene:create( event )
           align = 'center',
         }
         action_text:setFillColor(1, 1, 1, 1)
-        container:insert(action_text)      
+        top_container:insert(action_text)      
     end
-
-    wheel = getWheel()
     
     function listener:timer( event )
       -- this prevents multiple timers from running by stopping the previous active timer if present
@@ -234,15 +179,24 @@ function scene:create( event )
 
     local wheel_hitbox = display.newRect(0, 0, 320, 222 )  
     wheel_hitbox.isVisible, wheel_hitbox.isHitTestable = false, true
-    wheel_hitbox:addEventListener( "touch", wheelTouchListner )      
+    wheel_hitbox:addEventListener( "touch", wheelTouchListner )   
     
+    -------------------
+    -- WHEEL  PICKER --
+    -------------------    
     
-    container_xtra:insert(wheel)
-    container_xtra:insert(wheel_hitbox) -- it's not visible
+    wheel, targets = target_picker_wheel() 
+    wheel.top = -1*(bottom_container_h*0.5)
+    wheel.left = -1*(bottom_container_w*0.5)-15  -- not sure what the -15 is for?
+    wheel = widget.newPickerWheel(wheel)
     
+    bottom_container:insert(wheel)
+    bottom_container:insert(wheel_hitbox) -- it's not visible
+    
+    -- Action text needs to have the wheel setup before it can get the text (since the wheel selection returns the target/item/etc. strings)
     action_text = display.newText{
       text = getActionText(action),
-      width = container_w - 10, 
+      width = top_container_w - 10, 
       x = 5,
       y = -40,
       font = native.systemFont,
@@ -250,10 +204,10 @@ function scene:create( event )
       align = 'center',
     }
     action_text:setFillColor(1, 1, 1, 1)
-    container:insert(action_text)    
+    top_container:insert(action_text)    
    
-    sceneGroup:insert(container)
-    if container_xtra then sceneGroup:insert(container_xtra) end
+    sceneGroup:insert(top_container)
+    sceneGroup:insert(bottom_container)
 end
 
 -- "scene:show()"
