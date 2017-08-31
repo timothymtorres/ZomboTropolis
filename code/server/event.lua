035446fe57493e6be9514322618beaf425e8cd55
Timothy <timothymtorres@gmail.com>
2017-08-31 01:56:22 -0500

    Completely changed how broadcastEvent works.  Code is more simplier and shorter.
    The aim in doing this is to get the project ready to remove getMessage()
    descriptions and just insert descriptions of the events in outcomes or
    item/skill activiations.  Plus this overhaul should allow msgs to be
    sent even when an action hasn't taken place by the player.  (ie.  we
    need to send a serverwide msg)

--- code/server/event.lua
+++ code/server/event.lua
@@ -1,63 +1,36 @@
---[[  THIS IS OLD CODE
+local function broadcastEvent(zone, msg, setting) --settings={stage, range, mob_type}
+  local date = os.time()  
 
-local broadcast_zone = { --  (terminate, deactivate, cancel, blackkout)
-  self = {'move', 'search', 'enter', 'exit', 'discard'},
-  pair = {'attack', 'heal'},
-  stage = {'revive', 'death', 'destruction', 'speak'}, -- equipment destruction
-  tile = {'destruction'},  -- barricade destruction
-  area = {'flare', 'blackout'},
-}
-
--- setup our broadcast range for easier access
-for zone_type, event in pairs(broadcast_zone) do broadcast_zone[event] = zone_type end
-
-local function event(zone, action, data)  
-  broadcastEvent(zone, action, data)
-end
---]]
-
-local function event(zone, action, data)
-  local z_type, z_tile, z_stage, z_range = zone.type, zone.tile, zone.stage, zone.range  
-  local z_player, z_target = zone.player, zone.target
-  local date = os.time()
-  
-  if z_type == 'self' then
-    z_player.log:event(date, action, data, 1)
-  elseif z_type == 'pair' then
-    z_player.log:event(date, action, data, 1)
-    z_target.log:event(date, action, data, 2)
-  elseif z_type == 'stage' then
-    local players = z_tile:getPlayers(z_stage)
-    -- if player destroyed equipment or revived, isolate player for 1st person
-    for _, player in pairs(players) do 
-      if z_player and z_player == player then player.log:event(date, action, data, 1)
-      else player.log:event(date, action, data, 3) 
+  if zone:isType('player') then
+    local player = zone
+    player.log:insert(msg, date)
+  elseif zone:isType('tile') then
+    local tile = zone
+    local range, stage, mob_type = setting.range, setting.stage, setting.mob_type
+    
+    -- delivers msg to tile
+    local players = tile:getPlayers(stage)
+    for _, player in pairs(players) do
+      if (not mob_type or player:isMobType(mob_type) ) and player:isStanding() then 
+        player.log:insert(msg, date)
       end
-    end
-  elseif z_type == 'tile' then  
-    local tile_players = z_tile:getPlayers('all')
-    for _, player in pairs(tile_players) do  
---print('z_player is '..z_player, 'player is '..player)
-      if z_player and z_player == player then player.log:event(date, action, data, 1)
-      else player.log:event(date, action, data, 3) 
-      end      
-    end
-  elseif z_type == 'area' then
-    local map = z_tile:getMap()
-    local y_pos, x_pos = z_tile:getPos()
-    for y = y_pos - z_range, y_pos + z_range do
-      for x = x_pos - z_range, x_pos + z_range do
-        if map[y] and map[y][x] then
-          -- setting up a new localized zone thats params will be overwritten
-          -- z_type is now 'tile' for the next broadcastEvent() instead of 'area'
-          -- this avoids an infinite broadcastEvent() loop
-          local zone = zone
-          zone.type, zone.tile = 'tile', map[y][x]
-          event(zone, action, data)
+    end        
+      
+    if range then -- delivers msg to area of tiles (in the shape of a square that is [range] x sized)
+      local map = zone:getMap()
+      local y_pos, x_pos = tile:getPos()
+      for y = y_pos - range, y_pos + range do
+        for x = x_pos - range, x_pos + range do
+          if map[y] and map[y][x] and not (y_pos == y and x_pos == x) then -- avoid broadcast on map[y_pos][x_pos] since the msg has already been delievered to that tile 
+            broadcastEvent(map[y][x], msg, {stage=stage, mob_type=mob_type}) 
+          end
         end
       end
-    end
+    end      
+  elseif zone:isType('map') then
+    --broadcast msg to all players on that map
+  --elseif zone:isType('server') then   broadcast msg to all players on server?
   end
 end
 
-return event
+return broadcastEvent
