8c4429e863d3d3c2409df19d25ebe70d2dc53233
Timothy <timothymtorres@gmail.com>
2017-09-02 22:11:49 -0500

    Removed error_msg, result, zone, params, and broadcastEvent from the perform func.
    All of this has been restructured into different locations.
    
    -Error_msgs are broadcasted in criteria.
    -result and params are removed for now (but still exist in item/skill
    activation and outcome)
    -zone and broadcastEvent has been moved to calling the function in the
    item/skill activation and outcome

--- code/player/action/perform.lua
+++ code/player/action/perform.lua
@@ -15,35 +15,19 @@ end
 local function perform(action, player, ...)
   local mob_type = player:getMobType()
   local action_category = action_list[mob_type][action].category
-  -- standard checks on ALL actions
-  local basic_verification, error_msg = pcall(basicCriteria, player, action)   
-  -- checks to make sure action is valid
-  local verification, error_msg = pcall(serverCriteria[action_category], action, player, ...)
+
+  local basic_verification= pcall(basicCriteria, player, action) -- standard checks on ALL actions
+  local verification = pcall(serverCriteria[action_category], action, player, ...) -- checks to make sure action is valid
   
   if basic_verification and verification then
     local ap, AP_cost = player:getStat('ap'), player:getCost('ap', action)    
-    -- process the action and update stuff (returns a result dependent on action)
-    local result = serverOutcome[action_category](action, player, ...)
-    local zone = getZone(action, player, ...) -- may need to change location of getZone (for server events?)
-    local params = {player, ...}
-  
-    if result then -- merge our param table with result
-      for _, data in ipairs(result) do params[#params+1] = data end
-    end
-    -- creates a text event that is sent out to witness (aka players) message logs
-    broadcastEvent(zone, action, params)
-    
+    serverOutcome[action_category](action, player, ...) -- process the action
     player.condition:elapse(player, AP_cost)   
-    -- update our stats
     player:updateStat('ap', -1*AP_cost)
     player:updateStat('xp', AP_cost)
-  else 
-    local error_ID = error_list[error_msg]
-    player.log:event(os.time(), 'error', error_ID)
-    -- player:updateStat('IP', 1)
-print(error_msg)
-    error(error_msg)    
   end
+  
+  --player:updateStat('IP', 1)  -- IP connection hits?  Hmmmm?
 end
 
 return perform
