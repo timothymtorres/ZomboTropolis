e54456ccf833c6849e3c8cd5af00da9ef6888232
Timothy <timothymtorres@gmail.com>
2017-10-04 00:47:33 -0500

    Added event arg to broadcast funcs that passes event data to player logs

--- code/server/event.lua
+++ code/server/event.lua
@@ -1,6 +1,6 @@
 local broadcastEvent = {}
 
-function broadcastEvent.player(player, msg, self_msg)
+function broadcastEvent.player(player, msg, self_msg, event)  --this broadcasts specifically to exclude player and give player their own self_msg 
   local date = os.time()  
  
   local tile = player:getTile()
@@ -10,15 +10,15 @@ function broadcastEvent.player(player, msg, self_msg)
   for _, player_INST in pairs(players) do
     if player_INST:isStanding() and player_INST ~= player then 
       -- plug in map[y][x] coords into msg with string.gsub()
-      player_INST.log:insert(msg, date)
+      player_INST.log:insert(msg, event, date)
     end
   end
   
-  player.log:insert(self_msg, date)
+  player.log:insert(self_msg, event, date)
 end
     
 --settings={stage=inside/outside, range=num, mob_type=zombie/human, exclude={player=true, ...}}
-function broadcastEvent.zone(zone, msg, setting)
+function broadcastEvent.zone(zone, msg, event, setting)
   local date = os.time()  
   
   if zone:isClass('tile') then
@@ -30,7 +30,7 @@ function broadcastEvent.zone(zone, msg, setting)
     for _, player_INST in pairs(players) do
       if (not mob_type or player_INST:isMobType(mob_type) ) and player_INST:isStanding() and (not exclude or not exclude[player_INST]) then 
         -- plug in map[y][x] coords into msg with string.gsub()
-        player_INST.log:insert(msg, date)
+        player_INST.log:insert(msg, event, date)
       end
     end        
 
@@ -40,7 +40,7 @@ function broadcastEvent.zone(zone, msg, setting)
       for y = y_pos - range, y_pos + range do
         for x = x_pos - range, x_pos + range do
           if map[y] and map[y][x] and not (y_pos == y and x_pos == x) then -- avoid broadcast on map[y_pos][x_pos] since the msg has already been delievered to that tile 
-            broadcastEvent(map[y][x], msg, {stage=stage, mob_type=mob_type, exclude=exclude}) 
+            broadcastEvent(map[y][x], msg, event, {stage=stage, mob_type=mob_type, exclude=exclude}) 
           end
         end
       end
