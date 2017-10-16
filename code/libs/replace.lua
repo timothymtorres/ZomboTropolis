1ff133b931f268383d9a3b6c266727f62ac52037
Timothy <timothymtorres@gmail.com>
2017-10-15 20:51:03 -0500

    Changed replace func to return an empty string if passed as an param instead of returning a space concated with the string

--- code/libs/replace.lua
+++ code/libs/replace.lua
@@ -4,7 +4,7 @@ local gsub = string.gsub
 -- This function replaces '{}' patterns in a string with a replacment string
 local replace = function(str, repl)
   if type(repl) == 'string' then
-    return gsub(str, pattern, ' '..repl)  
+    return gsub(str, pattern, repl ~= '' and ' '..repl or '')  
   elseif type(repl) == 'table' then
     return gsub(str, pattern, function(s) return repl[s] and ' '..repl[s] or '' end)
   end
