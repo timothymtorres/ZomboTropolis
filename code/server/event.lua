ed09552bd11846f40a6f4468f3e57e0b1c8bf297
Timothy <timothymtorres@gmail.com>
2017-10-18 18:35:43 -0500

    Added exclude[tile] option, renamed zone to tile
    * broadcastEvent.zone has now been renamed to tile
    * broadcastEvent.tile can exclude a tile in the setting table
    * Made place holders for future code features to broadcast to map and
    server in commented out funcs below

--- code/server/event.lua
+++ code/server/event.lua
@@ -14,37 +14,36 @@ function broadcastEvent.player(player, msg, self_msg, event)  --this broadcasts 
   
   player.log:insert(self_msg, event)
 end
-    
---settings={stage=inside/outside, range=num, mob_type=zombie/human, exclude={player=true, ...}}
-function broadcastEvent.zone(zone, msg, event, setting)
-  if zone:isClass('tile') then
-    local tile = zone
-    local range, stage, mob_type, exclude = setting.range, setting.stage, setting.mob_type, setting.exclude
-    
-    -- delivers msg to tile
+
+--setting={stage=inside/outside, range=num, mob_type=zombie/human, exclude={player=true, tile=true, ...}}
+function broadcastEvent.tile(tile, msg, event, setting)
+  local range, stage, mob_type, exclude = setting.range, setting.stage, setting.mob_type, setting.exclude
+  
+  if not exclude or not exclude[tile] then 
     local players = tile:getPlayers(stage)
-    for _, player_INST in pairs(players) do
+    for _, player_INST in pairs(players) do -- deliver msg to tile
       if (not mob_type or player_INST:isMobType(mob_type) ) and player_INST:isStanding() and (not exclude or not exclude[player_INST]) then 
         -- plug in map[y][x] coords into msg with string.gsub()
         player_INST.log:insert(msg, event)
       end
     end        
+  end
 
-    if range then -- delivers msg to area of tiles (in the shape of a square that is [range] x sized)
-      local map = zone:getMap()
-      local y_pos, x_pos = tile:getPos()
-      for y = y_pos - range, y_pos + range do
-        for x = x_pos - range, x_pos + range do
-          if map[y] and map[y][x] and not (y_pos == y and x_pos == x) then -- avoid broadcast on map[y_pos][x_pos] since the msg has already been delievered to that tile 
-            broadcastEvent(map[y][x], msg, event, {stage=stage, mob_type=mob_type, exclude=exclude}) 
-          end
+  if range then -- delivers msg to area of tiles (in the shape of a square that is [range] x sized)
+    local map = tile:getMap()
+    local y_pos, x_pos = tile:getPos()
+    for y = y_pos - range, y_pos + range do
+      for x = x_pos - range, x_pos + range do
+        if map[y] and map[y][x] and not (y_pos == y and x_pos == x) then -- avoid broadcast on map[y_pos][x_pos] since the msg has already been delievered to that tile 
+          broadcastEvent(map[y][x], msg, event, {stage=stage, mob_type=mob_type, exclude=exclude}) 
         end
       end
-    end      
-  elseif zone:isClass('map') then
-    --broadcast msg to all players on that map
-  --elseif zone:isType('server') then   broadcast msg to all players on server?
-  end
+    end
+  end      
 end
+
+-- Add these later when needed
+--function broadcastEvent.map(map, msg, event, setting) end     
+--function broadcastEvent.server(server, msg, event, setting) end
 
 return broadcastEvent
