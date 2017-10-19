7282f73d5ffc43e27b15d9f7930389f7a7015f3b
Timothy <timothymtorres@gmail.com>
2017-10-19 01:16:08 -0500

    Removed deprecated modules and added error_msgs

--- code/player/action/perform.lua
+++ code/player/action/perform.lua
@@ -1,8 +1,5 @@
 local serverOutcome = require('code.player.action.outcome')
 local serverCriteria = require('code.player.action.criteria')
-local broadcastEvent = require('code.server.event')
-local getZone = require('code.player.action.zone')
-local error_list = require('code.error.list')
 local action_list = require('code.player.action.list')
  
 local function basicCriteria(player, action)
@@ -15,9 +12,11 @@ end
 local function perform(action, player, ...)
   local mob_type = player:getMobType()
   local action_category = action_list[mob_type][action].category
-
-  local basic_verification= pcall(basicCriteria, player, action) -- standard checks on ALL actions
-  local verification = pcall(serverCriteria[action_category], action, player, ...) -- checks to make sure action is valid
+  
+  -- standard check on ALL actions
+  local basic_verification, basic_error_msg = pcall(basicCriteria, player, action)
+  -- checks to make sure specific action is valid
+  local verification, error_msg = pcall(serverCriteria[action_category], action, player, ...) 
   
   if basic_verification and verification then
     local ap, AP_cost = player:getStat('ap'), player:getCost('ap', action)    
@@ -25,6 +24,8 @@ local function perform(action, player, ...)
     player.condition:elapse(player, AP_cost)   
     player:updateStat('ap', -1*AP_cost)
     player:updateStat('xp', AP_cost)
+  else -- Houston, we have a problem!
+    player.log:insert(basic_error_msg or error_msg)
   end
   
   --player:updateStat('IP', 1)  -- IP connection hits?  Hmmmm?
