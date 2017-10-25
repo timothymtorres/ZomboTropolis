85d588e9d3a5f9b55089794a63e416ff5dbf24d1
Timothy <timothymtorres@gmail.com>
2017-10-25 15:05:24 -0500

    Added functionality to accept class objects
    Takes a class object uses their tostring() method to return a string

--- code/libs/replace.lua
+++ code/libs/replace.lua
@@ -6,7 +6,11 @@ local replace = function(str, repl)
   if type(repl) == 'string' then
     return gsub(str, pattern, repl ~= '' and ' '..repl or '')  
   elseif type(repl) == 'table' then
-    return gsub(str, pattern, function(s) return repl[s] and (repl[s] ~= '' and ' '..repl[s]) or '' end)
+    if repl.class then 
+      return gsub(str, pattern, tostring(repl))
+    else -- just a regular table 
+      return gsub(str, pattern, function(s) return repl[s] and (repl[s] ~= '' and ' '..repl[s]) or '' end)
+    end
   end
 end
 
