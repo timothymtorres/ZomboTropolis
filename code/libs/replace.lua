7f083a870d1896dc7ee2d98d3c9b794d9f5efca9
Timothy <timothymtorres@gmail.com>
2017-10-25 15:50:50 -0500

    Refactored replace for optimization and clean code
    Moved anonymous function to module level scope.  Moved replacement_tbl
    to module level scope that gets overwritten by the replace function when
    it is called.  This enables the indexToStringSort function from only
    having to be created once, instead of the previous way the code was
    written which created the anonymous function every time the replace
    function was called!

--- code/libs/replace.lua
+++ code/libs/replace.lua
@@ -1,15 +1,29 @@
 local pattern = ' {(.-)}'
 local gsub = string.gsub
 
+local replacement_tbl
+
+local indexToStringSort = function(s)  -- converts index in replacement_tbl to string based on whether it is an object, nil, or string already  
+  local temp_str    
+  
+  if replacement_tbl[s] and replacement_tbl[s].class then temp_str = tostring(replacement_tbl[s])
+  elseif type(replacement_tbl[s]) == 'string' then temp_str = replacement_tbl[s]
+  elseif type(replacement_tbl[s]) == 'nil' then temp_str = ''
+  end
+
+  return temp_str == '' and temp_str or ' '..temp_str 
+end
+
 -- This function replaces '{}' patterns in a string with a replacment string
-local replace = function(str, repl)
-  if type(repl) == 'string' then
-    return gsub(str, pattern, repl ~= '' and ' '..repl or '')  
-  elseif type(repl) == 'table' then
-    if repl.class then 
-      return gsub(str, pattern, tostring(repl))
-    else -- just a regular table 
-      return gsub(str, pattern, function(s) return repl[s] and (repl[s] ~= '' and ' '..repl[s]) or '' end)
+local replace = function(str, replacement)
+  if type(replacement) == 'string' then
+    return gsub(str, pattern, replacement == '' and '' or ' '..replacement)  
+  elseif type(replacement) == 'table' then
+    if replacement.class then -- this is just a single class object
+      return gsub(str, pattern, tostring(replacement))
+    else -- just a regular table (with either strings or class objects inside)
+      replacement_tbl = replacement
+      return gsub(str, pattern, indexToStringSort)
     end
   end
 end
