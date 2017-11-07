42ef59eff13fe54fff39aa390e55ebe87f3f04a1
Timothy <timothymtorres@gmail.com>
2017-11-06 19:56:46 -0600

    Rename order to item and set table using item classes

---
+++ code/item/item.lua
@@ -0,0 +1,29 @@
+local Crowbar, Bat, Sledge, Knife, Katanna =                       unpack(require('code.item.melee_weaponry'))
+local Pistol, Magnum, Shotgun, Rifle, Flare, Molotov =             unpack(require('code.item.ranged_weaponry'))
+local FAK, Bandage, Syringe, Vaccine, Antidote =                   unpack(require('code.item.medical'))
+local Generator, Transmitter, Terminal, Fuel, Barricade, Toolbox = unpack(require('code.item.equipment'))
+local Radio, GPS, Flashlight, Sampler =                            unpack(require('code.item.gadget'))
+local Book, Bottle, Newspaper =                                    unpack(require('code.item.junk'))
+local Magazine, Shell, Clip, Quiver =                              unpack(require('code.item.ammo'))
+local Leather, Firesuit =                                          unpack(require('code.item.armor'))
+
+local Item = {
+  -- WEAPONRY
+  Crowbar, Bat, Sledge, Knife, Katanna, Pistol, Magnum, Shotgun, Rifle, Flare, Molotov, 
+  -- MEDICAL
+  FAK, Bandage, Syringe, Vaccine, Antidote, 
+  -- EQUIPMENT
+  Generator, Transmitter, Terminal, Fuel, Barricade, Toolbox,
+  -- GADGET
+  Radio, GPS, Flashlight, Sampler, --'cellphone', 'sampler'
+  -- JUNK
+  Book, Bottle, Newspaper,
+  -- AMMO
+  Magazine, Shell, Clip, Quiver,
+  -- ARMOR
+  Leather, Firesuit,
+}
+
+for Class in ipairs(Item) do Item[Class.name] = Class end
+
+return Item
