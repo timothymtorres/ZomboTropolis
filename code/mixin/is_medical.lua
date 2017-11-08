caee7fb654c7f922218385e3e5c8f7a0ba17854a
Timothy <timothymtorres@gmail.com>
2017-11-08 10:59:22 -0600

    Add IsMedical mixin

---
+++ code/mixin/is_medical.lua
@@ -0,0 +1,9 @@
+local IsMedical = {}
+
+function IsMedical:getAccuracy() return self.accuracy end
+
+function IsMedical:getDice() return self.dice end
+  
+function IsMedical:getGroup() return self.group end 
+  
+return IsMedical  
