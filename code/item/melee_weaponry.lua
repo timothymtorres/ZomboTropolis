a4d6ba0a7f2a94c1d518cd9127563624befbf9d3
Timothy <timothymtorres@gmail.com>
2017-11-08 00:41:03 -0600

    Add weapon constants to items and include mixin

--- code/item/melee_weaponry.lua
+++ code/item/melee_weaponry.lua
@@ -1,6 +1,7 @@
 local class =          require('code.libs.middleclass')
 local ItemBase =       require('code.item.item_base')
 local broadcastEvent = require('code.server.event')
+local isWeapon =       require('code.mixin.is_weapon')
 string.replace =       require('code.libs.replace')
 
 -- need to add designated_weapon var to items?  Or do this via mixins?
@@ -12,33 +13,53 @@ string.replace =       require('code.libs.replace')
 ---  BRUTE
 --]]
 
-local Crowbar = class('Crowbar', ItemBase)
+local Crowbar = class('Crowbar', ItemBase):include(isWeapon)
 
 Crowbar.FULL_NAME = 'crowbar'
 Crowbar.WEIGHT = 8
 Crowbar.DURABILITY = 25
-Crowbar.MASTER_SKILL = 'smacking'
 Crowbar.CATEGORY = 'military'
+
+Crowbar.ATTACK_STYLE = 'melee'
+Crowbar.DAMAGE_TYPE = 'blunt'
+Crowbar.GROUP = {'brute', 'light_brute'}
+Crowbar.DICE = '1d4'
+Crowbar.ACCURACY = 0.25
+Crowbar.CRITICAL = 0.05
 
 -------------------------------------------------------------------
 
-local Bat = class('Bat', ItemBase)
+local Bat = class('Bat', ItemBase):include(isWeapon)
 
 Bat.FULL_NAME = 'baseball bat'
 Bat.WEIGHT = 9
 Bat.DURABILITY = 15
-Bat.MASTER_SKILL = 'smacking'
 Bat.CATEGORY = 'military'
+
+Bat.ATTACK_STYLE = 'melee'
+Bat.DAMAGE_TYPE = 'blunt'
+Bat.GROUP = {'brute', 'light_brute'}
+Bat.DICE = '1d5'
+Bat.ACCURACY = 0.25
+Bat.CRITICAL = 0.05
+Bat.MASTER_SKILL = 'smacking'
 
 -------------------------------------------------------------------
 
-local Sledge = class('Sledge', ItemBase)
+local Sledge = class('Sledge', ItemBase):include(isWeapon)
 
 Sledge.FULL_NAME = 'sledgehammer'
 Sledge.WEIGHT = 25
 Sledge.DURABILITY = 20
-Sledge.MASTER_SKILL = 'smashing'
 Sledge.CATEGORY = 'military'
+
+Sledge.ATTACK_STYLE = 'melee'
+Sledge.DAMAGE_TYPE = 'blunt'
+Sledge.GROUP = {'brute', 'heavy_brute'}
+Sledge.DICE = '1d8'
+Sledge.ACCURACY = 0.25
+Sledge.CRITICAL = 0.025
+Sledge.MASTER_SKILL = 'smashing'
 
 -------------------------------------------------------------------
 
@@ -48,22 +69,53 @@ Sledge.CATEGORY = 'military'
 
 -------------------------------------------------------------------
 
-local Knife = class('Knife', ItemBase)
+local Knife = class('Knife', ItemBase):include(isWeapon)
 
 Knife.FULL_NAME = 'knife'
 Knife.WEIGHT = 3
 Knife.DURABILITY = 10
-Knife.MASTER_SKILL = 'slicing'
 Knife.CATEGORY = 'military'
+
+Knife.ATTACK_STYLE = 'melee'
+Knife.DAMAGE_TYPE = 'pierce'
+Knife.GROUP = {'blade', 'light_blade'}
+Knife.DICE = '1d2+1'
+Knife.ACCURACY = 0.25
+Knife.CRITICAL = 0.075
+Knife.MASTER_SKILL = 'slicing'
 
 -------------------------------------------------------------------
 
-local Katanna = class('Katanna', ItemBase)
+local Katanna = class('Katanna', ItemBase):include(isWeapon)
 
 Katanna.FULL_NAME = 'katanna'
 Katanna.WEIGHT = 7
 Katanna.DURABILITY = 15
-Katanna.MASTER_SKILL = 'chopping'
 Katanna.CATEGORY = 'military'
+
+Katanna.ATTACK_STYLE = 'melee'
+Katanna.DAMAGE_TYPE = 'pierce'
+Katanna.GROUP = {'blade', 'heavy_blade'}
+Katanna.DICE = '1d4+2'
+Katanna.ACCURACY = 0.25
+Katanna.CRITICAL = 0.10
+Katanna.MASTER_SKILL = 'chopping'
+
+--------------------------------------------------------------------
+
+-- axe? fireaxe?
+--[[
+weapon.fuel = {}
+weapon.fuel.full_name = 'fuel tank'
+weapon.fuel.attack_style = 'melee'
+weapon.fuel.damage_type = 'blunt'
+weapon.fuel.group = {'brute'}
+weapon.fuel.dice = '1d3'
+weapon.fuel.accuracy = 0.25
+weapon.fuel.critical = 0.025
+weapon.fuel.one_use = true
+weapon.fuel.combustion_source = false
+weapon.fuel.fuel_amount = {'1d5+10', '2d5+20', '3d5+30', '4d5+40'}
+--]]
 
 return {Crowbar, Bat, Sledge, Knife, Katanna}
--- code/item/ranged_weaponry.lua
+++ code/item/ranged_weaponry.lua
@@ -1,6 +1,7 @@
 local class =          require('code.libs.middleclass')
 local ItemBase =       require('code.item.item_base')
 local broadcastEvent = require('code.server.event')
+local isWeapon =       require('code.mixin.is_weapon')
 string.replace =       require('code.libs.replace')
 
 -- need to add designated_weapon var to items?  Or do this via mixins?
@@ -12,7 +13,7 @@ string.replace =       require('code.libs.replace')
 
 -------------------------------------------------------------------
 
-local Pistol = class('Pistol', ItemBase)
+local Pistol = class('Pistol', ItemBase):include(isWeapon)
 
 Pistol.FULL_NAME = 'pistol'
 Pistol.WEIGHT = 6
@@ -20,9 +21,19 @@ Pistol.DURABILITY = 40
 Pistol.MASTER_SKILL = 'light_guns'
 Pistol.CATEGORY = 'military'
 
+Pistol.ATTACK_STYLE = 'ranged'
+Pistol.DAMAGE_TYPE = 'bullet'
+Pistol.GROUP = {'guns', 'light_guns'}
+Pistol.DICE = '1d6+2'
+Pistol.ACCURACY = 0.30
+Pistol.CRITICAL = 0.05
+
+Pistol.MAX_AMMO = 14
+Pistol.RELOAD_AMOUNT = 14
+
 -------------------------------------------------------------------
 
-local Magnum = class('Magnum', ItemBase)
+local Magnum = class('Magnum', ItemBase):include(isWeapon)
 
 Magnum.FULL_NAME = 'magnum'
 Magnum.WEIGHT = 6
@@ -30,9 +41,19 @@ Magnum.DURABILITY = 50
 Magnum.MASTER_SKILL = 'light_guns'
 Magnum.CATEGORY = 'military'
 
+Magnum.ATTACK_STYLE = 'ranged'
+Magnum.DAMAGE_TYPE = 'bullet'
+Magnum.GROUP = {'guns', 'light_guns'}
+Magnum.DICE = '1d9+4'
+Magnum.ACCURACY = 0.30
+Magnum.CRITICAL = 0.05
+
+Magnum.MAX_AMMO = 6
+Magnum.RELOAD_AMOUNT = 6
+
 -------------------------------------------------------------------
 
-local Shotgun = class('Shotgun', ItemBase)
+local Shotgun = class('Shotgun', ItemBase):include(isWeapon)
 
 Shotgun.FULL_NAME = 'shotgun'
 Shotgun.WEIGHT = 10
@@ -40,9 +61,19 @@ Shotgun.DURABILITY = 40
 Shotgun.MASTER_SKILL = 'heavy_guns'
 Shotgun.CATEGORY = 'military'
 
+Shotgun.ATTACK_STYLE = 'ranged'
+Shotgun.DAMAGE_TYPE = 'bullet'
+Shotgun.GROUP = {'guns', 'heavy_guns'}
+Shotgun.DICE = '3d3++1'
+Shotgun.ACCURACY = 0.30
+Shotgun.CRITICAL = 0.05
+
+Shotgun.MAX_AMMO = 2
+Shotgun.RELOAD_AMOUNT = 1
+
 -------------------------------------------------------------------
 
-local Rifle = class('Rifle', ItemBase)
+local Rifle = class('Rifle', ItemBase):include(isWeapon)
 
 Rifle.FULL_NAME = 'assualt rifle'
 Rifle.WEIGHT = 15
@@ -50,15 +81,34 @@ Rifle.DURABILITY = 40
 Rifle.MASTER_SKILL = 'heavy_guns'
 Rifle.CATEGORY = 'military'
 
+Rifle.ATTACK_STYLE = 'ranged'
+Rifle.DAMAGE_TYPE = 'bullet'
+Rifle.GROUP = {'guns', 'heavy_guns'}
+Rifle.DICE = '(1d5)x3'
+Rifle.ACCURACY = 0.30
+Rifle.CRITICAL = 0.05
+
+Rifle.MAX_AMMO = 5
+Rifle.RELOAD_AMOUNT = 5
+
 -------------------------------------------------------------------
 
-local Flare = class('Flare', ItemBase)
+local Flare = class('Flare', ItemBase):include(isWeapon)
 
 Flare.FULL_NAME = 'flare'
 Flare.WEIGHT = 5
 Flare.DURABILITY = 0
 Flare.MASTER_SKILL = 'explosives'
 Flare.CATEGORY = 'military'
+
+Flare.ATTACK_STYLE = 'ranged'
+Flare.DAMAGE_TYPE = 'scorch'
+Flare.GROUP = {'guns'}  -- really?!
+Flare.DICE = '5d3'
+Flare.ACCURACY = 0.15
+Flare.CRITICAL = 0.30
+--Flare.combustion_source = true
+--Flare.fuel_amount = {'1d2+3', '1d3+5', '1d4+8', '1d5+10'}
 
 function Flare.client_criteria(player)
   assert(player:isStaged('outside'), 'Player must be outside to use flare')  
@@ -110,13 +160,22 @@ end
 
 -------------------------------------------------------------------
 
-local Molotov = class('Molotov', ItemBase)
+local Molotov = class('Molotov', ItemBase):include(isWeapon)
 
 Molotov.FULL_NAME = 'molotov cocktail'
 Molotov.WEIGHT = 5
 Molotov.DURABILITY = 0
 Molotov.MASTER_SKILL = 'explosives'
 Molotov.CATEGORY = 'military'
+
+Molotov.ATTACK_STYLE = 'ranged'
+Molotov.DAMAGE_TYPE = 'scorch'
+Molotov.GROUP = {'special'}
+Molotov.DICE = '5d2'
+Molotov.ACCURACY = 0.20
+Molotov.CRITICAL = 0.30
+--Molotov.combustion_source = true
+--Molotov.fuel_amount = {'1d5+10', '1d7+12', '1d9+14', '2d5+15'}
 
 -------------------------------------------------------------------
 
@@ -128,6 +187,18 @@ bow.WEIGHT = 9
 missle = {}
 missle.FULL_NAME = 'missle launcher'
 missle.WEIGHT = 25
+
+weapon.missile = {}
+weapon.missile.full_name = 'missile launcher'
+weapon.missile.attack_style = 'ranged'
+weapon.missile.damage_type = 'scorch'
+weapon.missile.group = {'special'}
+weapon.missile.dice = '5d8'
+weapon.missile.accuracy = 0.15
+weapon.missile.critical = 0.30
+weapon.missile.one_use = true
+
+bow/crossbow
 --]]
 
 return {Pistol, Magnum, Shotgun, Rifle, Flare, Molotov}
