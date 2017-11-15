56d460bcbd839f0185b0033eb78b65c0b1feca9d
Timothy <timothymtorres@gmail.com>
2017-11-14 20:50:28 -0600

    Remove print statements from chanceToHit

--- code/player/chanceToHit.lua
+++ code/player/chanceToHit.lua
@@ -14,31 +14,17 @@ local function chanceToHit(player, target, weapon)
 
   -- skill bonus for melee/ranged attack style
   for skill in pairs(skill_attack_bonus[mob_type][attack_style]) do
-    if player.skills:check(skill) then skill_bonus = skill_bonus + skill_attack_bonus[mob_type][attack_style][skill] 
---print('Bonus Attack Style Acquired - ['..skill..'] = '..skill_attack_bonus[mob_type][attack_style][skill])  
-    end
+    if player.skills:check(skill) then skill_bonus = skill_bonus + skill_attack_bonus[mob_type][attack_style][skill] end
   end
-  
---print('attack style skill bonus = '..skill_bonus)  
   
   -- skill bonus for weapon categories
   for _, weapon_type in ipairs(weapon_groups) do
---print('combat - weapon_type', weapon_type)
     for skill in pairs(skill_attack_bonus[mob_type][weapon_type]) do
---print(skill, flags[skill])
-      if player.skills:check(skill) then skill_bonus = skill_bonus + skill_attack_bonus[mob_type][weapon_type][skill] 
---print('Bonus Weapon Group Acquired - ['..skill..'] = '..skill_attack_bonus[mob_type][weapon_type][skill])          
-      end
+      if player.skills:check(skill) then skill_bonus = skill_bonus + skill_attack_bonus[mob_type][weapon_type][skill] end
     end
   end
-  
---print('weapon category skill bonus = '..skill_bonus)   
-  
 
-  
   local attack_chance = base_chance + skill_bonus + condition_bonus
---print('attack_chance='..attack_chance, 'skill bonus='..skill_bonus..'/condition bonus='..condition_bonus..'/base_chance='..base_chance)
---print('dice.chance('..attack_chance..') =', dice.chance(attack_chance))
 
   -- if attacking building or equipment (from the outside) there is a severe penalty, the to-hit chance is halved
   if target:getClassName() ~= 'player' and player:isStaged('outside') then attack_chance = attack_chance*0.5 end 
