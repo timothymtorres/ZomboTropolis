d5ac0c00328c78edded8151d9fdc3d1fd62ff321
Timothy <timothymtorres@gmail.com>
2017-11-19 16:08:00 -0600

    Change building to Building and tile to TileBase

--- code/location/building/class.lua
+++ code/location/building/class.lua
@@ -1,6 +1,5 @@
 local class = require('code.libs.middleclass')
-local b_list = require('code.location.building.list')
-local tile = require('code.location.tile.class')
+local TileBase = require('code.location.tile.tile_base')
 local equipment = require('code.location.building.equipment.class')
 local generator = require('code.location.building.equipment.generator.class')
 local transmitter = require('code.location.building.equipment.transmitter.class')
@@ -10,9 +9,9 @@ local barricade = require('code.location.building.barrier.barricade.class')
 local integrity = require('code.location.building.integrity.class')
 local buildDesc = require('code.location.building.buildDesc')
 
-local building = class('building', tile)
+local Building = class('Building', TileBase)
 
-function building:initialize(map, y, x, name)
+function Building:initialize(map, y, x, name)
   tile.initialize(self, map, y, x, name)  
   self.inside_players = {}
 
@@ -27,27 +26,27 @@ function building:initialize(map, y, x, name)
   self.terminal    = terminal:new(self) 
 end
 
-function building:insert(player, setting) 
+function Building:insert(player, setting) 
   if setting == 'inside' then self.inside_players[player] = player 
   elseif setting == 'outside' or setting == nil then self.outside_players[player] = player
   end
 end
 
-function building:remove(player, setting) 
+function Building:remove(player, setting) 
   if setting == 'inside' then self.inside_players[player] = nil  
   elseif setting == 'outside' or setting == nil then self.outside_players[player] = nil 
   end
 end
 
-function building:install(equipment_type, condition) self[equipment_type]:install(condition) end
+function Building:install(equipment_type, condition) self[equipment_type]:install(condition) end
 
-function building:blackout()
+function Building:blackout()
   -- 3x3 area do blackout event on tile(s)
 end
 
-function building:getBarrier() return (self.barricade:getHP() > 0 and 'barricade') or (self.door:getHP() > 0 and 'door') end  -- should this be a class?
+function Building:getBarrier() return (self.barricade:getHP() > 0 and 'barricade') or (self.door:getHP() > 0 and 'door') end  -- should this be a class?
 
-function building:getBarrierDesc() 
+function Building:getBarrierDesc() 
   local cade_str, space_str = self.barricade:getDesc()
   local door_str = self.door:getDesc()
   
@@ -66,7 +65,7 @@ function building:getBarrierDesc()
   return door_desc..cade_desc..space_desc
 end
 
-function building:getEquipment()
+function Building:getEquipment()
   local machines = {}
   for machine in pairs(equipment.subclasses) do 
     if self[machine]:isPresent() then machines[#machines+1] = tostring(machine) end  -- should this be a class?
@@ -74,9 +73,9 @@ function building:getEquipment()
   return machines
 end
 
--- function building:getPos() return (NO NEED?)
+-- function Building:getPos() return (NO NEED?)
 
-function building:isPresent(setting)
+function Building:isPresent(setting)
   if setting == 'equipment' then
     for machine, i in pairs(equipment.subclasses) do
       if self[tostring(machine)]:isPresent() then return true end
@@ -90,18 +89,18 @@ function building:isPresent(setting)
   end
 end
 
--- function building:isOccupied()  great way to check for people inside when ruining
+-- function Building:isOccupied(mob_type)  great way to check for people inside when ruining
 
-function building:isFortified() return self.barricade:getHP() > 0 and self.door:getHP() > 0 end
+function Building:isFortified() return self.barricade:getHP() > 0 and self.door:getHP() > 0 end
 
-function building:isPowered() return self.generator:isActive() end
+function Building:isPowered() return self.generator:isActive() end
 
-function building:isOpen() return self.barricade:isDestroyed() and (self.door:isOpen() or self.door:isDestroyed()) end
+function Building:isOpen() return self.barricade:isDestroyed() and (self.door:isOpen() or self.door:isDestroyed()) end
 
 -- do we need this?!
-function building:isDescPresent(desc_type) return (self[desc_type] and #self[desc_type] > 0) or false end
+function Building:isDescPresent(desc_type) return (self[desc_type] and #self[desc_type] > 0) or false end
 
-function building:updateHP(num) 
+function Building:updateHP(num) 
   if self:getBarrier() == 'barricade' then 
     self.barricade:updateHP(num) 
   else -- self:getBarrier() == 'door'
@@ -109,22 +108,4 @@ function building:updateHP(num)
   end
 end
 
-function building:dataToClass(...) -- this should be a middleclass function (fix later)
-  local combined_lists = {...}
-  for _, list in ipairs(combined_lists) do
-    for obj in pairs(list) do
-      self[obj] = class(obj, self)
-    --[  by removing this the initialize function is now defaulted to building.initialize
-      self[obj].initialize = function(self_subclass, map, y, x, building_name)
-        self.initialize(self_subclass, map, y, x, building_name)
-      end
-    --]]  
-      for field, data in pairs(list[obj]) do self[obj][field] = data end
-    end
-  end
-end
-
--- turn our list of buildings into building class
-building:dataToClass(b_list)
-
-return building
+return Building
