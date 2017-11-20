9e235e035bce2916b9c1f11b263a383161fcefd8
Timothy <timothymtorres@gmail.com>
2017-11-19 18:19:26 -0600

    Add locations to tile table

--- code/location/tile/tile.lua
+++ code/location/tile/tile.lua
@@ -1,10 +1,34 @@
-local Street, Cemetary, Monument, Park, Wasteland, Stadium, Junkyard, Carpark = unpack(require('code.location.tile.outside_areas'))
+local Street, Cemetary, Wasteland =  unpack(require('code.location.tile.barren')) 
+local Carpark, Junkyard =            unpack(require('code.location.tile.scrap'))
+local Monument, Park, Stadium =      unpack(require('code.location.tile.tourist'))
+local Apartment, Hotel =             unpack(require('code.location.building.residential'))
+local Library, School, College =     unpack(require('code.location.building.public'))
+local Warehouse, Factory =           unpack(require('code.location.building.industrial'))
+local PD, FD, Hospital, Courthouse = unpack(require('code.location.building.government'))
+local News, Sport, Bar, Mall =       unpack(require('code.location.building.commercial'))
 
 local Tile = {
-  -- OUTSIDE AREAS
-  Street, Cemetary, Monument, Park, Wasteland, Stadium, Junkyard, Carpark,
+---- OUTSIDE ----
+
+  -- BARREN
+  Street, Cemetary, Wasteland,
+  -- SCRAP 
+  Carpark, Junkyard,
+  -- TOURIST
+  Monument, Park, Stadium,
+
+---- INSIDE ----
+
   -- RESIDENTIAL
-  
+  Apartment, Hotel,
+  -- PUBLIC
+  Library, School, College,
+  -- INDUSTRIAL
+  Warehouse, Factory,
+  -- GOVERNMENT
+  PD, FD, Hospital, Courthouse,
+  -- COMMERCIAL
+  News, Sport, Bar, Mall,
 }
 
 for _, Class in ipairs(Tile) do 
