8b239e1e116fdba14dc6d7cb11545d1e951c3b0d
Timothy <timothymtorres@gmail.com>
2017-11-27 18:54:20 -0600

    Move zombie ability criteria code to generic

--- code/player/zombie/ability/generic.lua
+++ code/player/zombie/ability/generic.lua
@@ -12,11 +12,25 @@ function drag_prey.client_criteria(player)
   --assert(p_tile:isOpen(), 'Building must have an open exit to drag prey')  
 end
 
+function drag_prey.server_criteria(player)
+  local p_tile, setting = player:getTile(), player:getStage()
+  local human_n = p_tile:countPlayers('human', setting)
+  assert(setting == 'inside', 'Must be inside building to drag prey')  
+  assert(human_n > 0, 'Must have humans nearby to drag')
+  --assert(p_tile:isOpen(), 'Building must have an open exit to drag prey')
+end
+
 -------------------------------------------------------------------
 
 local groan = {name='groan'}
 
 function groan.client_criteria(player)
+  local p_tile = player:getTile()
+  local human_n = p_tile:countPlayers('human', player:getStage())
+  assert(human_n > 0, 'Must have humans nearby to groan')
+end
+
+function groan.server_criteria(player)
   local p_tile = player:getTile()
   local human_n = p_tile:countPlayers('human', player:getStage())
   assert(human_n > 0, 'Must have humans nearby to groan')
@@ -30,6 +44,12 @@ function gesture.client_criteria(player)
   local p_tile = player:getTile()
   local player_n = p_tile:countPlayers('all', player:getStage())
   assert(player_n - 1  > 0, 'Must have players nearby to gesture')
+end
+
+function gesture.server_criteria(player)
+  local p_tile = player:getTile()
+  local player_n = p_tile:countPlayers('all', player:getStage())
+  assert(player_n - 1 > 0, 'Must have players nearby to gesture')
 end
 
 -------------------------------------------------------------------
@@ -47,12 +67,26 @@ function armor.client_criteria(player)
   assert(player.armor:hasRoomForLayer(), 'No remaining room for additional armor layers')
 end
 
+function armor.server_criteria(player, armor_type)
+  -- Must recheck EP cost since armor_type not specified in basic server_criteria EP assert, causing it to be skipped
+  local cost, ep = player:getCost('ep', armor_type), player:getStat('ep')
+  assert(ep >= cost, 'Not enough enzyme points to use skill')  
+  
+  local skill = organic_armor_list[armor_type].required_skill
+  assert(player.skills:check(skill), 'Must have required skill to use armor ability')
+  assert(player.armor:hasRoomForLayer(), 'No remaining room for additional armor layers')
+end
+
 -------------------------------------------------------------------
 
 local ransack = {name='ransack'}
 
 function ransack.client_criteria(player)
-  assert(player:isStaged('inside'), 'Must be inside to ruin building')
+  assert(player:isStaged('inside'), 'Must be inside to ransack building')
+end
+
+function ransack.server_criteria(player)
+  assert(player:isStaged('inside'), 'Must be inside to ransack building')
 end
 
 -------------------------------------------------------------------
@@ -60,6 +94,10 @@ end
 local mark_prey = {name='mark_prey'}
 
 function mark_prey.client_criteria(player)
+  
+end
+
+function mark_prey.server_criteria(player)
   
 end
 
@@ -71,6 +109,10 @@ function track.client_criteria(player)
    assert(player:isStaged('outside'), 'Must be outside to track prey')   
 end
 
+function track.server_criteria(player)
+  assert(player:isStaged('outside'), 'Must be outside to track prey')  
+end
+
 -------------------------------------------------------------------
 
 local acid = {name='acid'}
@@ -80,5 +122,12 @@ function acid.client_criteria(player)
   local human_n = p_tile:countPlayers('human', player:getStage())
   assert(human_n > 0, 'Must have humans nearby to use acid')  
 end
+
+function acid.server_criteria(player)
+  local p_tile = player:getTile()
+  local human_n = p_tile:countPlayers('human', player:getStage())
+  assert(human_n > 0, 'Must have humans nearby to use acid')  
+end
+
 
 return client_criteria
