cd63bbd5eb90478dd2491ca10f43d7939cda3680
Timothy <timothymtorres@gmail.com>
2017-12-07 10:02:51 -0600

    Remove Fist from zombie organic weaponry file

--- code/player/zombie/organic_weaponry.lua
+++ code/player/zombie/organic_weaponry.lua
@@ -1,23 +1,6 @@
 local class =          require('code.libs.middleclass')
 local IsWeapon =       require('code.item.mixin.is_weapon')
 
-local Fist = class('Fist'):include(IsWeapon)
-
-Fist.FULL_NAME = 'fist'
-
-Fist.weapon = {
-  ATTACK_STYLE = 'melee',
-  DAMAGE_TYPE = 'blunt',
-  GROUP = {'hand'},
-  DICE = '1d1',
-  ACCURACY = 0.20,
-  CRITICAL = 0.05,
-  ORGANIC = 'human',
-  MASTER_SKILL = 'martial_arts_adv',
-}
-
-function Fist:__tostring() return tostring(self.class) end
-
 -------------------------------------------------------------------
 
 local Claw = class('Claw'):include(IsWeapon)
@@ -72,4 +55,4 @@ weapon.acid.CRITICAL = 0.30
 weapon.acid.ORGANIC = 'zombie'
 --]]
 
-return {Fist, Claw, Bite}
+return {Claw, Bite}
