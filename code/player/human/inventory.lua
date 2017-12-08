2074dee7ff76a237b6bbfa3c7d457d41555d634c
Timothy <timothymtorres@gmail.com>
2017-12-07 22:13:02 -0600

    Add updateDurability func to inventory

--- code/player/human/inventory.lua
+++ code/player/human/inventory.lua
@@ -1,7 +1,5 @@
 local class = require('code.libs.middleclass')
 
-local inventory = class('code.player.inventory')
-
 --[[
   player.inventory = {
     [1] = crowbar,
@@ -11,8 +9,9 @@ local inventory = class('code.player.inventory')
   }
 --]]
 
-function inventory:initialize() 
-  self.radio_receivers = {}  
+function inventory:initialize(player)
+  self = {player = player}
+  --self.radio_receivers = {}  
 end
 
 function inventory:insert(item_INST) self[#self+1] = item_INST end  
@@ -30,7 +29,7 @@ end
 
 function inventory:search(item_name)
   for inv_ID, item_INST in ipairs(self) do
-    if item_INST:getClassName() == item_name then 
+    if item_INST:getName() == item_name then 
       return true, inv_ID 
     end
   end
@@ -43,6 +42,26 @@ function inventory:catalog()
   return contents
 end
 
+function inventory:updateDurability(inv_ID)
+  local itemObj = self[inv_ID] 
+  local player = self.player
+  
+  if itemObj:isSingleUse() then 
+    self:remove(inv_ID) 
+  elseif itemObj:failDurabilityCheck(player) then 
+    local condition = itemObj:updateCondition(-1)
+
+    if condition <= 0 then
+      player.log:append('Your '..tostring(itemObj)..' is destroyed!')
+      self:remove(inv_ID)
+    elseif itemObj:isConditionVisible(player) then
+      player.log:append('Your '..tostring(itemObj)..' degrades to a '..itemObj:getConditionState()..' state.')  
+    end    
+  end
+end
+
+--[[ This code is deprecated and needs to be rewritten
+
 function inventory:updateRadio(player, mode, freq)  
   local receivers = self.radio_receivers
   
@@ -50,5 +69,6 @@ function inventory:updateRadio(player, mode, freq)
   elseif mode == 'remove' then receivers[freq] = receivers[freq] - 1 end
   channel[mode](channel, freq, player)
 end
+--]]
 
 return inventory
