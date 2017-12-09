9ed632bde9f1dc7b356ae3ca603b8f5fe0f59a9b
Timothy <timothymtorres@gmail.com>
2017-12-09 13:56:47 -0600

    Remove deprecataed code from EquipmentBase

--- code/location/building/equipment/equipment_base.lua
+++ code/location/building/equipment/equipment_base.lua
@@ -1,26 +1,21 @@
 local class = require('code.libs.middleclass')
 
 local Equipment = class('EquipmentBase')
-local max_hp = 7
+local MAX_HP = 8
 
 function EquipmentBase:initialize(building) 
-  self.hp = 0
+  self.hp = MAX_HP
   self.building = building
 end
 
-function EquipmentBase:install() self.hp = max_hp end
-
 function EquipmentBase:destroy()
-  self.hp = 0
   -- update building stuff... (ie. power out if generator, player notification event)
 end
 
 function EquipmentBase:updateHP(num)
   self.hp = math.min(math.max(self.hp + num, 0), max_hp) 
-  if self.hp <= 0 then self:destroy() end  
+  if self.hp == 0 then self:destroy() end  
 end
-
-function EquipmentBase:isPresent() return self.hp > 0 end
 
 function EquipmentBase:getHP() return self.hp end
 
@@ -30,19 +25,6 @@ function EquipmentBase:getBuilding() return self.building end
   self.generator = 0        -- 1 bit {exist}, 3 bit {08hp}, 4 bit {fuel} 
   self.transmitter = 0      -- 1 bit {exist}, 3 bit {08hp}, 10 bit {radio freq}
   self.terminal = 0         -- 1 bit {exist}, 3 bit {08hp},   
-  
-  equipment flags
-  
-  building.equipment:getFlags()
-  building.barricade:getFlags()
-  
-  anyway
-  
-  generator = {
-    exist = 1,
-    see that's the problem... NO FLAGS... just fields?  hmmmmmm not a flag system,
-    you are compressing/decompressing the data....... then if that's the case
-  }
 --]]
 
 return EquipmentBase
