93d0813dd3c09541c81dc08a4c9c45db7fd64c2f
Timothy <timothymtorres@gmail.com>
2017-12-09 21:15:53 -0600

    Remove impale from entangle code
    Made entangle:add use self

--- code/player/human/condition/entangle.lua
+++ code/player/human/condition/entangle.lua
@@ -2,15 +2,16 @@ local class = require('code.libs.middleclass')
 
 local entangle = class('entangle')
 
-function entangle:initialize()
-  self.grapple, self.impale = false, false  
+function entangle:initialize(player)
+  self.player = player
+  self.grapple = false  
 end
 
 function entangle:remove()
   local tangled_player = self.grapple
   if tangled_player then
-    self.grapple, self.impale = false, false
-    tangled_player.condition.entangle.grapple, tangled_player.condition.entangle.impale = false, false
+    self.grapple = false
+    tangled_player.condition.entangle.grapple = false
   end
 end
 
@@ -18,18 +19,9 @@ function entangle:isActive()
   return (self.grapple and true) or false
 end
 
-function entangle:isImpaleActive() return self.impale end
-
--- Below are functions to be called from module itself, not player.condition.entangle
-
-function entangle.isTangledTogether(player_a, player_b)
-  local grapple_a, grapple_b = player_a.condition.entangle.grapple, player_b.condition.entangle.grapple
-  return grapple_a == player_b and grapple_b == player_a
-end
-
-function entangle.add(player_a, player_b, impale_bonus)
-  player_a.condition.entangle.grapple, player_b.condition.entangle.grapple = player_b, player_a
-  player_a.condition.entangle.impale = player_a.condition.entangle.impale or impale_bonus
+function entangle:add(target)
+  self.grapple = target
+  target.condition.entangle.grapple = self.player
 end
 
 return entangle
--- code/player/zombie/condition/entangle.lua
+++ code/player/zombie/condition/entangle.lua
@@ -2,15 +2,16 @@ local class = require('code.libs.middleclass')
 
 local entangle = class('entangle')
 
-function entangle:initialize()
-  self.grapple, self.impale = false, false  
+function entangle:initialize(player)
+  self.player = player
+  self.grapple = false  
 end
 
 function entangle:remove()
   local tangled_player = self.grapple
   if tangled_player then
-    self.grapple, self.impale = false, false
-    tangled_player.condition.entangle.grapple, tangled_player.condition.entangle.impale = false, false
+    self.grapple = false
+    tangled_player.condition.entangle.grapple = false
   end
 end
 
@@ -18,13 +19,9 @@ function entangle:isActive()
   return (self.grapple and true) or false
 end
 
-function entangle:isImpaleActive() return self.impale end
-
--- Below are functions to be called from module itself, not player.condition.entangle
-
-function entangle.add(player_a, player_b, impale_bonus)
-  player_a.condition.entangle.grapple, player_b.condition.entangle.grapple = player_b, player_a
-  player_a.condition.entangle.impale = player_a.condition.entangle.impale or impale_bonus
+function entangle:add(target)
+  self.grapple = target
+  target.condition.entangle.grapple = self.player
 end
 
 return entangle
