bfa74973940389e544d26961822b198c3bf68cf3
Timothy <timothymtorres@gmail.com>
2017-12-10 21:33:28 -0600

    Change infection class capitalization

--- code/player/human/status_effect/infection.lua
+++ code/player/human/status_effect/infection.lua
@@ -1,23 +1,19 @@
 local class = require('code.libs.middleclass')
 local dice = require('code.libs.dice')
 
-local infection = class('infection')
+local Infection = class('Infection')
 
-local MAX_TIME = 511
-local INCUBATION_DEFAULT = '1d1' --'1d80+20'
 local INFECTION_DAMAGE_PER_TICK = -1
 
-function infection:initialize(player)
+function Infection:initialize(player)
   self.player = player
 end
 
-function infection:isImmune() return self.incubation_timer > 0 end
-
-function infection:elapse(player, time)  
+function Infection:elapse(player, time)  
   if not self.player.status_condition:isActive('immune') then
     player.log:append('You feel the infection spreading.')    
     player:updateStat('hp', INFECTION_DAMAGE_PER_TICK*time)    
   end
 end
 
-return infection
+return Infection
