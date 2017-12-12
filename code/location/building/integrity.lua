db6462f64d9a1e91343b346d2e111711477b2573
Timothy <timothymtorres@gmail.com>
2017-12-11 23:42:22 -0600

    Move integrity to building folder

--- code/location/building/integrity.lua
+++ code/location/building/integrity.lua
@@ -1,18 +1,18 @@
 local class = require('code.libs.middleclass')
 local dice = require('code.libs.dice')
 
-local integrity = class('integrity')
+local Integrity = class('Integrity')
 
 local BUILDING_MAX_HP = 20 --{15, 20, 30, ???} this is a HP table based on building sizes
 local RANSACK_VALUE = BUILDING_MAX_HP - 10
 local MAX_DECAY = -60
 
-function integrity:initialize(building)
+function Integrity:initialize(building)
   self.building = building
   self.hp = BUILDING_MAX_HP -- Update this later to the size of the building (all buildilng are 1 tile sized right now)
 end
 
-function integrity:updateHP(num)
+function Integrity:updateHP(num)
   self.hp = math.min(math.max(self.hp+num, MAX_DECAY), BUILDING_MAX_HP)
   
   if self.hp >= 0 or self.hp == MAX_DECAY then
@@ -24,7 +24,7 @@ function integrity:updateHP(num)
   self:updateDesc() 
 end
 
-function integrity:canModify(player)  -- possibly move all or parts of this code to criteria.toolbox and critera.ransack?
+function Integrity:canModify(player)  -- possibly move all or parts of this code to criteria.toolbox and critera.ransack?
   local n_humans = self.building:countPlayers('human', 'inside')
   local n_zombies = self.building:countPlayers('zombie', 'inside')
   
@@ -42,17 +42,17 @@ function integrity:canModify(player)  -- possibly move all or parts of this code
   return false
 end
 
-function integrity:isState(setting) return self:getState() == setting end
+function Integrity:isState(setting) return self:getState() == setting end
 
-function integrity:getState() 
+function Integrity:getState() 
   local integrity_is_full = self.hp == BUILDING_MAX_HP
   local integrity_within_ransack_range = self.hp >= 0 
   local integrity_within_ruin_range = self.hp < 0
   return (integrity_is_full and 'intact') or (integrity_within_ransack_range and 'ransacked') or (integrity_within_ruin_range and 'ruined') 
 end
 
---function integrity:getHP() return self.hp end
+--function Integrity:getHP() return self.hp end
 
-function integrity:updateDesc() end
+function Integrity:updateDesc() end
 
-return integrity
+return Integrity
