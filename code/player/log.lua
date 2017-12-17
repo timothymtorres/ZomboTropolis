145366c03b3e92536e65d81b1fc155202f72403d
Timothy <timothymtorres@gmail.com>
2017-12-16 20:45:05 -0600

    Rename variable event to incident in Log:read

--- code/player/log.lua
+++ code/player/log.lua
@@ -16,13 +16,9 @@ function Log:insert(msg, event, date)
   }
 end
 
-function Log:append(msg)
-  self[#self].msg = self[#self].msg..'  '..msg
-end
+function Log:append(msg) self[#self].msg = self[#self].msg..'  '..msg end
 
-function Log:reset()
-  for i=1, #self do self[i] = nil end
-end
+function Log:reset() for i=1, #self do self[i] = nil end end
 
 local second = 1
 local minute = second*60
@@ -50,18 +46,21 @@ local function getTimeStamp(date)  -- time in seconds
   return '('..amount..' '..unit..suffix..' ago)'
 end
 
-
 function Log:read()
   local list = {}
-  for _, event in ipairs(self) do  -- sort this by day
-    local date = os.date('%x', event.date)
+  for _, incident in ipairs(self) do  -- sort this by day
+    local date = os.date('%x', incident.date)
     local index
+
+    -- splits our list into indexes based date
     if #list == 0 then index = 1
     else index = (date == os.date('%x', list[#list].date) and #list) or #list+1 
     end
-    list[index] = list[index] or {date=event.date, collapsed=true, events={}} 
+
+    list[index] = list[index] or {date=incident.date, collapsed=true, events={}}  -- create an empty index if no events present
+    
     local event_tbl = list[index].events
-    event_tbl[#event_tbl+1] = event.msg..' '..getTimeStamp(event.date) 
+    event_tbl[#event_tbl+1] = incident.msg..' '..getTimeStamp(incident.date) 
   end
   return list
 end
