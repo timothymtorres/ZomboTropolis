bce1d90964c640044d4e8bc25e27141073a46696
Timothy <timothymtorres@gmail.com>
2017-12-23 20:24:06 -0600

    Add hide status_effect to zombie

--- code/player/zombie/status_effect/effects.lua
+++ code/player/zombie/status_effect/effects.lua
@@ -1,8 +1,9 @@
-local Entangle =  require('code.player.status_effect.entangle')
-local Burn = 	  require('code.player.zombie.status_effect.burn')
-local Track =     require('code.player.zombie.status_effect.track')
+local Entangle = require('code.player.status_effect.entangle')
+local Burn = require('code.player.zombie.status_effect.burn')
+local Track = require('code.player.zombie.status_effect.track')
+local Hide = require('code.player.zombie.status_effect.hide')	  
 
-local effects = {Entangle, Burn, Track}
+local effects = {Entangle, Burn, Track, Hide}
 
 for _, Class in ipairs(effects) do
   effects[Class.name] = Class
