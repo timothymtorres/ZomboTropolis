0eed21104d9dbf76b8d0b93a43863b54e23e993d
Timothy <timothymtorres@gmail.com>
2017-12-28 22:11:36 -0600

    Remove old armor code

--- code/work-in-progress/armor/is_armor.lua
+++
@@ -1,8 +0,0 @@
-local IsArmor = {}
-
-function IsArmor:getProtection(damage_type)
-  local resistance, condition = self.armor.resistance, self.condition
-  return (resistance[condition] and resistance[condition][damage_type]) or resistance[damage_type] or 0 
-end
-
-return IsArmor
