46192e834c9777fa714ab52493fcc2681d584da8
Timothy <timothymtorres@gmail.com>
2017-12-31 15:07:26 -0600

    Add isDamaged function to Door class

--- code/location/tile/building/door.lua
+++ code/location/tile/building/door.lua
@@ -2,20 +2,18 @@ local class = require('code.libs.middleclass')
 local Barrier = require('code.location.tile.building.barrier')
 
 local Door = class('Door', Barrier)  
-local default_hp, max_hp = 3, 3
+local DEFAULT_HP, MAX_HP = 3, 3
 
 local door_desc = {[0] = 'destroyed', [1] = 'smashed', [2] = 'dented', [3] = 'undamaged'}
 
 function Door:initialize()
   Barrier.initialize(self)
   
-  self.hp = default_hp
+  self.hp = DEFAULT_HP
   self.is_open = false
   
-  self.hp_desc = door_desc[default_hp]
+  self.hp_desc = door_desc[DEFAULT_HP]
  end 
- 
- Door.max_hp = max_hp
 
 function Door:updateDesc() self.hp_desc = door_desc[self.hp] end
 
@@ -26,6 +24,8 @@ function Door:repair() self:updateHP(3) end
 function Door:toggle() self.is_open = not self.is_open end
 
 function Door:isDestroyed() return self.hp == 0 end
+
+function Door:isDamaged() return self.hp == MAX_HP
 
 function Door:isOpen() return self.is_open end
 
