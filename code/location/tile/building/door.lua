8cd9e6de2faa64b42d8b52836b7fb81bf0199827
Timothy <timothymtorres@gmail.com>
2017-12-31 15:08:22 -0600

    Remove deprecated Desc code from Door class

--- code/location/tile/building/door.lua
+++ code/location/tile/building/door.lua
@@ -4,20 +4,12 @@ local Barrier = require('code.location.tile.building.barrier')
 local Door = class('Door', Barrier)  
 local DEFAULT_HP, MAX_HP = 3, 3
 
-local door_desc = {[0] = 'destroyed', [1] = 'smashed', [2] = 'dented', [3] = 'undamaged'}
-
 function Door:initialize()
   Barrier.initialize(self)
   
   self.hp = DEFAULT_HP
   self.is_open = false
-  
-  self.hp_desc = door_desc[DEFAULT_HP]
  end 
-
-function Door:updateDesc() self.hp_desc = door_desc[self.hp] end
-
-function Door:getDesc() return self.hp_desc end
 
 function Door:repair() self:updateHP(3) end
 
