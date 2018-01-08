8edae2fedf5a27fe2269520d0b31d9988cc7c5c8
Timothy <timothymtorres@gmail.com>
2018-01-07 20:13:15 -0600

    Remove Limb from player

--- game/code/player/player.lua
+++ game/code/player/player.lua
@@ -5,7 +5,6 @@ local broadcastEvent = require('code.server.event')
 local catalogAvailableActions = require('code.player.catalog')
 local chanceToHit = require('code.player.chanceToHit')
 local Equipment = require('code.player.equipment')
-local Limb = require('code.player.limb')
 
 local Player = class('Player')
 
@@ -30,7 +29,6 @@ function Player:initialize(username, map_zone, y, x) --add account name
   self.log = Log:new()
   self.status_effect = StatusEffect:new(self)
   self.equipment = Equipment:new(self)
-  self.limb = Limb:new(self)
 
   map_zone[y][x]:insert(self)
 end
