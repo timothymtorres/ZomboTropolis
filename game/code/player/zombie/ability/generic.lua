7bb2c3dede34a2491968199fa4186d60a89ea5b5
Timothy <timothymtorres@gmail.com>
2018-01-09 17:15:44 -0600

    Add armor criteria/activation code to ability

--- game/code/player/zombie/ability/generic.lua
+++ game/code/player/zombie/ability/generic.lua
@@ -1,5 +1,6 @@
 local dice = require('code.libs.dice')
 local broadcastEvent = require('code.server.event')
+local organic_armor = require('code.player.zombie.organic_armor')
 
 -------------------------------------------------------------------
 
@@ -193,17 +194,97 @@ end
 
 -------------------------------------------------------------------
 
-local armor = {name='armor', ap={cost=1}}
+local armor = {name='armor', ap={cost=5, modifier={armor_adv = -2}}
 
-local MIN_EP_ARMOR_COST = 1  -- this is a bit of a hack
+function armor:client_criteria(player)
+  assert(player.skills:check('armor'), 'Must have "armor" skill to create armor')
 
-function armor.client_criteria(player)
+  local p_tile = player:getTile()
+  local p_stage = player:getStage()
+  local corpses = p_tile:getCorpses(p_stage)
+  assert(corpses, 'No available corpses to eat')
+  
+  local edible_corpse_present 
+  for corpse in pairs(corpses) do
+    if corpse:isMobType('human') and corpse.carcass:edible(player) then
+      edible_corpse_present = true
+      break
+    end
+  end
+  assert(edible_corpse_present, 'All corpses have been eaten')  
 end
 
-function armor.server_criteria(player, armor_type)
+function armor:server_criteria(player, armor_type)
+  assert(player.skills:check('armor'), 'Must have "armor" skill to create armor')
+  assert(not armor_type or player.skills:check('armor_adv'), 'Must have "armor_adv" skill to select armor')  
+
+  local p_tile = player:getTile()
+  local p_stage = player:getStage()
+  local corpses = p_tile:getCorpses(p_stage)
+  assert(corpses, 'No available corpses to eat')
+  
+  local edible_corpse_present 
+  for corpse in pairs(corpses) do
+    if corpse:isMobType('human') and corpse.carcass:edible(player) then
+      edible_corpse_present = true
+      break
+    end
+  end
+  assert(edible_corpse_present, 'All corpses have been eaten')  
 end
 
-function armor.activate(player, armor_type)
+function armor:activate(player, armor_type)
+  local corpses = p_tile:getCorpses(player:getStage())
+  local target
+  local lowest_scavenger_num = 4
+  
+  -- finds the corpse with the lowest number of scavengers (fresh meat) 
+  for corpse in pairs(corpses) do
+    if corpse:isMobType('human') and corpse.carcass:edible(player) then
+      local corpse_scavenger_num = #corpse.carcass.carnivour_list
+      if lowest_scavenger_num > corpse_scavenger_num then
+        target = corpse
+        lowest_scavenger_num = corpse_scavenger_num 
+      end
+    end
+  end
+
+  local nutrition_LV = target.carcass:devour(player)
+  local armor_dice = dice:new('1d'..nutrition_LV)
+
+  if player.skills:check('armor_adv') then armor_dice = armor_dice ^ 1 end
+  local condition = armor_dice:roll()
+
+  local Armor = armor_type or organic_armor[math.random(1, #organic_armor)]
+  player.equipment:add('armor', Armor:new(condition))
+  
+  --------------------------------------------
+  -----------   M E S S A G E   --------------
+  --------------------------------------------
+  
+  local msg = 'You mutate a corpse and gain a {armor}.'
+  msg = msg:replace(self) -- This should work? Needs to be tested
+
+--[[  Planning on redoing the armor code so these msgs will probably change  
+  if armor_type == 'scale' then
+    msg[1] = 'Your skin shapeshifts into hard scales that are resistant to bladed weapons.'  
+  elseif armor_type == 'blubber' then
+    msg[1] = 'Your body shapeshifts into a blubbery mass that is skilled at absorbing blunt weapon impacts.'
+  elseif armor_type == 'gel' then
+    msg[1] = 'Your body oozes goo out of various pores that is effective at absorbing heat.'
+  elseif armor_type == 'sticky' then
+    msg[1] = 'Your body secretes slime causing the various armor layers to become more resillent.'
+  elseif armor_type == 'stretch' then
+    msg[1] = 'Your skin becomes loose and rubbery causing projectiles to slow as they make contact.'
+  end
+--]]  
+
+  --------------------------------------------
+  ---------   B R O A D C A S T   ------------
+  --------------------------------------------
+  
+  local event = {'armor', player}  
+  player.log:insert(msg, event)  
 end
 
 -------------------------------------------------------------------
--- game/code/player/zombie/organic_armor.lua
+++ game/code/player/zombie/organic_armor.lua
@@ -5,106 +5,7 @@ local IsArmor = require('code.item.mixin.is_armor')
 
 -------------------------------------------------------------------
 
-local OrganicArmor = class('OrganicArmor', Item):include(IsArmor)  -- remove Item superclass... do we need the methods?
-OrganicArmor.ap = {cost = 5, modifier={armor_adv = -2}}
-OrganicArmor.list = {}
-
-function OrganicArmor:client_criteria(player)
-	assert(player.skills:check('armor'), 'Must have "armor" skill to create armor')
-
-  local p_tile = player:getTile()
-  local p_stage = player:getStage()
-  local corpses = p_tile:getCorpses(p_stage)
-  assert(corpses, 'No available corpses to eat')
-  
-  local edible_corpse_present 
-  for corpse in pairs(corpses) do
-    if corpse:isMobType('human') and corpse.carcass:edible(player) then
-      edible_corpse_present = true
-      break
-    end
-  end
-  assert(edible_corpse_present, 'All corpses have been eaten')  
-end
-
-function OrganicArmor:server_criteria(player, armor_type)
-	assert(player.skills:check('armor'), 'Must have "armor" skill to create armor')
-  assert(not armor_type or player.skills:check('armor_adv'), 'Must have "armor_adv" skill to select armor')  
-
-  local p_tile = player:getTile()
-  local p_stage = player:getStage()
-  local corpses = p_tile:getCorpses(p_stage)
-  assert(corpses, 'No available corpses to eat')
-  
-  local edible_corpse_present 
-  for corpse in pairs(corpses) do
-    if corpse:isMobType('human') and corpse.carcass:edible(player) then
-      edible_corpse_present = true
-      break
-    end
-  end
-  assert(edible_corpse_present, 'All corpses have been eaten')  
-end
-
-function OrganicArmor:activate(player, armor_type)
-  local corpses = p_tile:getCorpses(player:getStage())
-  local target
-  local lowest_scavenger_num = 4
-  
-  -- finds the corpse with the lowest number of scavengers (fresh meat) 
-  for corpse in pairs(corpses) do
-    if corpse:isMobType('human') and corpse.carcass:edible(player) then
-      local corpse_scavenger_num = #corpse.carcass.carnivour_list
-      if lowest_scavenger_num > corpse_scavenger_num then
-        target = corpse
-        lowest_scavenger_num = corpse_scavenger_num 
-      end
-    end
-  end
-
-  local nutrition_LV = target.carcass:devour(player)
-  local armor_dice = dice:new('1d'..nutrition_LV)
-
-  if player.skills:check('armor_adv') then armor_dice = armor_dice ^ 1 end
-  local condition = armor_dice:roll()
-
-  armor_type = armor_type or OrganicArmor.list[math.random(1, #OrganicArmor.list)]
-  armor = OrganicArmor.subclass[armor]:new(condition) -- This should work... 
-
-  player.equipment:add('armor', armor)
-  
-  --------------------------------------------
-  -----------   M E S S A G E   --------------
-  --------------------------------------------
-  
-  local msg = 'You mutate a corpse and gain a {armor}.'
-  msg = msg:replace(self) -- This should work? Needs to be tested
-
---[[  Planning on redoing the armor code so these msgs will probably change  
-  if armor_type == 'scale' then
-    msg[1] = 'Your skin shapeshifts into hard scales that are resistant to bladed weapons.'  
-  elseif armor_type == 'blubber' then
-    msg[1] = 'Your body shapeshifts into a blubbery mass that is skilled at absorbing blunt weapon impacts.'
-  elseif armor_type == 'gel' then
-    msg[1] = 'Your body oozes goo out of various pores that is effective at absorbing heat.'
-  elseif armor_type == 'sticky' then
-    msg[1] = 'Your body secretes slime causing the various armor layers to become more resillent.'
-  elseif armor_type == 'stretch' then
-    msg[1] = 'Your skin becomes loose and rubbery causing projectiles to slow as they make contact.'
-  end
---]]  
-
-  --------------------------------------------
-  ---------   B R O A D C A S T   ------------
-  --------------------------------------------
-  
-  local event = {'armor', player}  
-  player.log:insert(msg, event)  
-end
-
--------------------------------------------------------------------
-
-local Scale = class('Scale', OrganicArmor)
+local Scale = class('Scale', Item):include(IsArmor)
 OrganicArmor.list[#OrganicArmor.list+1] = 'Scale'
 
 Scale.FULL_NAME = 'scale'
@@ -120,7 +21,7 @@ Scale.armor.resistance = {
 
 -------------------------------------------------------------------
 
-local Blubber = class('Blubber', OrganicArmor)
+local Blubber = class('Blubber', Item):include(IsArmor)
 OrganicArmor.list[#OrganicArmor.list+1] = 'Blubber'
 
 Blubber.FULL_NAME = 'blubber'
@@ -136,7 +37,7 @@ Blubber.armor.resistance = {
 
 ------------------------- ------------------------------------------
 
-local Gel = class('Gel', OrganicArmor)
+local Gel = class('Gel', Item):include(IsArmor)
 OrganicArmor.list[#OrganicArmor.list+1] = 'Gel'
 
 Gel.FULL_NAME = 'gel'
@@ -152,7 +53,7 @@ Gel.armor.resistance = {
 
 -------------------------------------------------------------------
 
-local Bone = class('Bone', OrganicArmor)
+local Bone = class('Bone', Item):include(IsArmor)
 OrganicArmor.list[#OrganicArmor.list+1] = 'Bone'
 
 Bone.FULL_NAME = 'bone'
