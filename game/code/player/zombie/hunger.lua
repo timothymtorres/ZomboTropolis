6f25b7f1ae280aa77762abe50f30352aeeb1566c
Timothy <timothymtorres@gmail.com>
2018-01-13 15:00:07 -0600

    Add high hunger threshold for satiation_bonus skill

--- game/code/player/zombie/hunger.lua
+++ game/code/player/zombie/hunger.lua
@@ -3,7 +3,7 @@ local dice = require('code.libs.dice')
 
 local Hunger = class('Hunger')
 
-local MAX_HUNGER = 1023
+local MAX_HUNGER, MAX_HUNGER_BONUS = 800, 1000
 
 function Hunger:initialize(player)
   self.player = player
@@ -12,15 +12,16 @@ end
 
 function Hunger:update(amt) 	
   self.hunger = math.max(self.hunger + amt, 0)
+  local hunger_threshold = player.skills:check('satiation_bonus') and MAX_HUNGER_BONUS or MAX_HUNGER
 
-  if self.hunger >= MAX_HUNGER then
-  	for i=1, MAX_HUNGER - self.hunger do 
+  if self.hunger >= hunger_threshold then
+  	for i=1, hunger_threshold - self.hunger do 
 	  	if dice.roll(10) == 1 then
 	  	  return self.player:starve() -- game over pal
 	  	end
     end
     
-	  self.hunger = MAX_HUNGER
+	  self.hunger = hunger_threshold
 	  local player = self.player   	  
 	  player.log:append('You are starving!')
   end
