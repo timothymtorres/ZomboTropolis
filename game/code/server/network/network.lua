2e566c81ab69c2c66ec995645bfe47fbab2db02e
Timothy <timothymtorres@gmail.com>
2018-01-17 17:12:23 -0600

    Fix Network:transmit ignore speaker in listener list

--- game/code/server/network/network.lua
+++ game/code/server/network/network.lua
@@ -4,7 +4,7 @@ local Network = class('Network')
 
 function Network:initialize() end
 
-function Network:insert(listener, channel) self[channel][listener] = listener end
+function Network:insert(listener, channel) self[channel][listener] = listener end  -- probably need to set this to a weak table
 
 function Network:remove(listener, channel) self[channel][listener] = nil end
 
@@ -12,11 +12,11 @@ function Network:check(listener, channel) return self[channel][listener] end
 
 function Network:transmit(speaker, channel, message)
   local event, settings = {'network', speaker, channel}, {stage='inside'}
-  local msg = tostring(speaker)..' ('..channel..'): '..message
+  local msg = tostring(speaker)..' ('..channel..'): '..message -- in UD the speaker is omitted It's [channel]: "Message here"
 
   for listener in pairs(self[channel]) do 
   	if tostring(listener) == 'building' then listener:broadcastEvent(msg, event, settings)
-  	else listener.log:insert(msg, event) -- listener is a player then
+  	elseif listener ~= speaker then listener.log:insert(msg, event)
   	end
   end
 end
