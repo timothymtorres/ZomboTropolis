79da793b171db05d89262098287f2a4c918054f0
Timothy <timothymtorres@gmail.com>
2018-01-17 17:13:33 -0600

    Add network subsystem to Zombie class

--- game/code/player/zombie/network.lua
+++ game/code/player/zombie/network.lua
@@ -3,14 +3,21 @@ local ZombieNetwork = require('code.server.network.zombie_network')
 
 local Network = class('Network')
 
-function Network:initialize()
+function Network:initialize(player)
+  self.player = player
   self.channel = 1
 end
 
 function Network:update(channel)
-  ZombieNetwork:remove(self.channel, player)
-  ZombieNetwork:add(channel, player)
+  ZombieNetwork:remove(self.player, self.channel)
+  ZombieNetwork:add(self.player, channel)
   self.channel = channel 
 end
+
+function Network:check(channel) return ZombieNetwork:check(self.player, channel) end
+
+function Network:transmit(msg) ZombieNetwork:transmit(self.player, self.channel, msg) end
+
+function Network:getChannelName(channel) return 'blah' end -- Setup a list of star constellations to return?
 
 return Network
