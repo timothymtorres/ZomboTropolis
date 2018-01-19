085d85c054d4d7b8c41b35f424d0f62e6b0cabe8
Timothy <timothymtorres@gmail.com>
2018-01-18 20:52:46 -0600

    Change Network to ServerNetwork with zombie/human

--- game/code/server/network/network.lua
+++ game/code/server/network/network.lua
@@ -1,24 +1,47 @@
 local class = require('code.libs.middleclass')
 
-local Network = class('Network')
+local ServerNetwork = class('ServerNetwork')
 
-function Network:initialize() end
+function ServerNetwork:initialize() 
+  self.zombie = {}
+  for channel=1, 16 do self.zombie[channel] = {} end  -- zombies only have a limited # of channels
 
-function Network:insert(listener, channel) self[channel][listener] = listener end  -- probably need to set this to a weak table
+  self.human = {}
+  for channel=1, 1024 do self.human[channel] = {} end
+end
 
-function Network:remove(listener, channel) self[channel][listener] = nil end
+local function getMob(listener)
+ 	if listener:isInstanceOf('player') then return listener:getMobType()
+	else listener:isInstanceOf('building') return 'human' -- transmitters only apply to human channels 
+	end
+end
 
-function Network:check(listener, channel) return self[channel][listener] end
+function ServerNetwork:insert(listener, channel)
+	local mob = getMob(listener)
+	self[mob][channel][listener] = listener -- probably need to set this to a weak table	
+end
 
-function Network:transmit(speaker, channel, message)
+function ServerNetwork:remove(listener, channel)
+	local mob = getMob(listener)
+	self[mob][channel][listener] = nil
+end
+
+function ServerNetwork:check(listener, channel) 
+	local mob = getMob(listener)
+	return self[mob][channel][listener] 
+end
+
+function ServerNetwork:transmit(speaker, channel, message)
   local event, settings = {'network', speaker, channel}, {stage='inside'}
-  local msg = tostring(speaker)..' ('..channel..'): '..message -- in UD the speaker is omitted It's [channel]: "Message here"
+  local mob = getMob(speaker)
+  local msg = tostring(speaker)..' ('..channel..'): '..message -- in UD the speaker is omitted from the broadcasted msg
 
-  for listener in pairs(self[channel]) do 
+  for listener in pairs(self[mob][channel]) do 
+  	-- tostring(listener) == 'building' is probably broken
   	if tostring(listener) == 'building' then listener:broadcastEvent(msg, event, settings)
   	elseif listener ~= speaker then listener.log:insert(msg, event)
   	end
   end
 end
 
-return Network
+return ServerNetwork
