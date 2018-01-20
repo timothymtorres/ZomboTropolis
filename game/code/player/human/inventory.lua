60b2583f3a9475c95eb1491729c0bc213942fd72
Timothy <timothymtorres@gmail.com>
2018-01-20 13:30:57 -0600

    Change Inventory args to handle instances/inv_IDs

--- game/code/player/human/inventory.lua
+++ game/code/player/human/inventory.lua
@@ -3,24 +3,35 @@ local class = require('code.libs.middleclass')
 local Inventory = class('Inventory')
 
 -- Inventory methods search, check, and lookup are all similiarly named.  Might want to be more speicfic or merge them into same method?
-
 function Inventory:initialize(player)
   self.player = player
-  --self.radio_receivers = {}  
 end
 
-function Inventory:insert(item_INST) self[#self+1] = item_INST end  
+function Inventory:insert(item) 
+  self[#self+1] = item 
+  self[item] = #self
+end
 
-function Inventory:remove(inv_ID) table.remove(self, inv_ID) end
+function Inventory:remove(obj)  -- obj can be inv_ID or item_INST
+  local inv_ID = type(obj) == 'number' and obj or self[obj]
+  local item = type(obj) == 'table' and obj or self[inv_ID]
 
-function Inventory:check(inv_ID) return (self[inv_ID] and true) or false end
+  -- put item:destroy() here?
+  self[item] = nil 
+  table.remove(self, inv_ID)
 
-function Inventory:lookup(inv_ID)  -- get itemClass_INST
+  -- update the inv_IDs
+  for i, item_INST in ipairs(self) do self[item_INST] = i end
+end
+
+function Inventory:check(inv_ID) return (self[inv_ID] and true) or false end  -- this method should be renamed to isPresent()
+
+function Inventory:lookup(inv_ID)  -- get itemClass_INST   this method should be renamed to getItem()
   if inv_ID == nil or self[inv_ID] == nil then return error('Inventory:lookup invalid') end  
   return self[inv_ID]
 end
 
-function Inventory:search(item_name)
+function Inventory:search(item_name)  -- needs to be renamed or deprecated (this searches by strings, mainly used for flashlights)
   for inv_ID, item_INST in ipairs(self) do
     if item_INST:getName() == item_name then 
       return true, inv_ID 
@@ -35,8 +46,10 @@ function Inventory:catalog()
   return contents
 end
 
-function Inventory:updateDurability(inv_ID, num)
-  local item, player = self[inv_ID], self.player
+function Inventory:updateDurability(obj, num)
+  local inv_ID = type(obj) == 'number' and obj or self[obj]
+  local item = type(obj) == 'table' and obj or self[inv_ID]
+  local player = self.player
   local is_single_use = item:isSingleUse()
   local failed_durability_test = not is_single_use and item:failDurabilityCheck(player)
   local condition 
