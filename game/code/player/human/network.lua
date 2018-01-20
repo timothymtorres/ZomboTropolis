3b69555cd2bee53d70f0e156f69dc79a0501ee84
Timothy <timothymtorres@gmail.com>
2018-01-19 22:08:08 -0600

    Add Network subsystem for Human Class

---
+++ game/code/player/human/network.lua
@@ -0,0 +1,23 @@
+local class = require('code.libs.middleclass')
+local ServerNetwork = require('code.server.network')
+
+local Network = class('Network')
+
+function Network:initialize(player)
+  self.player = player
+  self.channel = {}
+end
+
+function Network:add(channel, radio) 
+	self.channels[channel] = radio
+	ServerNetwork:insert(self.player, channel) 
+end
+
+function Network:remove(channel) 
+	self.channels[channel] = nil
+	ServerNetwork:remove(self.player, channel) 
+end
+
+function Network:check(channel) return self.channel[channel] end
+
+return Network
