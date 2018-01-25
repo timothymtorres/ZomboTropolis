fbb52a062050acbd8441b37482210db27a00acdd
Timothy <timothymtorres@gmail.com>
2018-01-24 19:46:50 -0600

    Fix Scanner item not added to items list

--- game/code/item/items.lua
+++ game/code/item/items.lua
@@ -1,6 +1,6 @@
 local Crowbar, Bat, Sledge, Knife, Katanna = unpack(require('code.item.items.melee_weaponry'))
 local Pistol, Magnum, Shotgun, Rifle, Flare, Molotov = unpack(require('code.item.items.ranged_weaponry'))
-local FAK, Bandage, Syringe, Vaccine, Antidote = unpack(require('code.item.items.medical'))
+local FAK, Bandage, Syringe, Vaccine, Antidote, Scanner = unpack(require('code.item.items.medical'))
 local Generator, Transmitter, Terminal = unpack(require('code.item.items.machinery'))
 local Fuel, Barricade, Toolbox = unpack(require('code.item.items.tools'))
 local Radio, GPS, Flashlight, Sampler = unpack(require('code.item.items.gadget'))
