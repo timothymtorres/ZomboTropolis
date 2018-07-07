159bddd1fe893bc4fcfab91b53d5623787114956
Timothy <timothymtorres@gmail.com>
2018-07-07 16:39:33 -0500

    Add apc, generator, terminal, transmitter templates
    These are used to manipulate sprite states

---
+++ game/scenes/objects/transmitter.lua
@@ -0,0 +1,33 @@
+
+-- Module/class for platfomer hero
+
+-- Use this as a template to build an in-game mob
+
+-- Define module
+local M = {}
+local composer = require( "composer" )
+
+function M.new( object )	
+	if not object then error( "ERROR: Expected display visual" ) end
+
+	local transmitter = object:getVisual() 
+
+	function transmitter:install()
+		self:setSequence('transmitter-off')
+		self:show()		
+	end
+
+	function transmitter:destroy()
+		self:setSequence('transmitter-off')
+		self:hide()
+	end
+
+	function transmitter:setPower(setting)
+		self:setSequence('transmitter-'..setting)
+		self:play()
+	end
+
+	return transmitter
+end
+
+return M
