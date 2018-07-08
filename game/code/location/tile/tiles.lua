44433a50d1bcf8d05ee085aec0ccea5a676d7639
Timothy <timothymtorres@gmail.com>
2018-07-08 00:42:40 -0500

    Add new buildings/tiles to list

--- game/code/location/tile/tiles.lua
+++ game/code/location/tile/tiles.lua
@@ -1,11 +1,13 @@
-local Land, Street, Cemetary, Wasteland, Runway, Forest = unpack(require('code.location.tile.tiles.barren')) 
+local Land, Street, Cemetary, Wasteland, Runway, Forest, Obstical= unpack(require('code.location.tile.tiles.barren')) 
 local Carpark, Junkyard = unpack(require('code.location.tile.tiles.scrap'))
 local Monument, Park, Stadium = unpack(require('code.location.tile.tiles.tourist'))
 local Apartment = unpack(require('code.location.tile.building.buildings.residential'))
-local Library, News = unpack(require('code.location.tile.building.buildings.public'))
-local Warehouse, Factory = unpack(require('code.location.tile.building.buildings.industrial'))
+local Library, News, Museum, Church = unpack(require('code.location.tile.building.buildings.public'))
+local Warehouse, Factory, Power, Control = unpack(require('code.location.tile.building.buildings.industrial'))
 local PD, FD, Hospital, Lab = unpack(require('code.location.tile.building.buildings.government'))
-local Sport, Bar, Mall = unpack(require('code.location.tile.building.buildings.commercial'))
+local Gas, Shop, Mall = unpack(require('code.location.tile.building.buildings.commercial'))
+local Bar, Sport = unpack(require('code.location.tile.buillding.buildings.entertainment'))
+local Gatehouse, Infirmary, Armory, Supply, Barracks = unpack(require('code.location.tile.building.buildings.military'))
 
 local Tile = {
   Land, Street, Cemetary, Wasteland,  
@@ -13,7 +15,10 @@ local Tile = {
   Monument, Park, Stadium, Apartment,
   Library, News, Warehouse, Factory, 
   PD, FD, Hospital, Lab,
-  Sport, Bar, Mall,
+  Power, Control, Sport, Bar,
+  Church, Museum, Gas, Shop, 
+  Mall, Gatehouse, Infirmary, Armory,
+  Supply, Barracks, Obstical
 
   --[[
   do only 1 type (and then when they purchase naming rights have a list of subtypes names)
@@ -29,8 +34,6 @@ local Tile = {
 
   bar, church, sport, gas,
   museum, pawn
-
-
 
 
   power, control tower, ? yes (resource building relevant to a region)
