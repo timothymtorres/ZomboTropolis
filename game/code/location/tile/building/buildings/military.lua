dc643166062ec1dd96c69862a0242d607240e6d2
Timothy <timothymtorres@gmail.com>
2018-07-08 00:42:02 -0500

    Add military buildings

---
+++ game/code/location/tile/building/buildings/military.lua
@@ -0,0 +1,76 @@
+local class = require('code.libs.middleclass')
+local Building = require('code.location.tile.building.building')
+
+-------------------------------------------------------------------
+
+local Gatehouse = class('Gatehouse', Building)
+
+Gatehouse.FULL_NAME = 'gatehouse'
+Gatehouse.BUILDING_TYPE = 'military'
+Gatehouse.search_odds = {OUTSIDE = Building.search_odds.OUTSIDE, INSIDE = 0.30}
+Gatehouse.item_chance = {
+  outside = Building.item_chance.outside,
+  inside = {
+    Newspaper = 1.00,
+  }
+}
+
+-------------------------------------------------------------------
+
+local Infirmary = class('Infirmary', Building)
+
+Infirmary.FULL_NAME = 'infirmary'
+Infirmary.BUILDING_TYPE = 'military'
+Infirmary.search_odds = {OUTSIDE = Building.search_odds.OUTSIDE, INSIDE = 0.30}
+Infirmary.item_chance = {
+  outside = Building.item_chance.outside,
+  inside = {
+    Newspaper = 1.00,
+  }
+}
+
+-------------------------------------------------------------------
+
+local Armory = class('Armory', Building)
+
+Armory.FULL_NAME = 'armory'
+Armory.BUILDING_TYPE = 'military'
+Armory.search_odds = {OUTSIDE = Building.search_odds.OUTSIDE, INSIDE = 0.30}
+Armory.item_chance = {
+  outside = Building.item_chance.outside,
+  inside = {
+    Newspaper = 1.00,
+  }
+}
+
+-------------------------------------------------------------------
+
+local Supply = class('Supply', Building)
+
+Supply.FULL_NAME = 'supply'
+Supply.BUILDING_TYPE = 'military'
+Supply.search_odds = {OUTSIDE = Building.search_odds.OUTSIDE, INSIDE = 0.30}
+Supply.item_chance = {
+  outside = Building.item_chance.outside,
+  inside = {
+    Newspaper = 1.00,
+  }
+}
+
+-------------------------------------------------------------------
+
+local Barracks = class('Barracks', Building)
+
+Barracks.FULL_NAME = 'barracks'
+Barracks.BUILDING_TYPE = 'military'
+Barracks.search_odds = {OUTSIDE = Building.search_odds.OUTSIDE, INSIDE = 0.30}
+Barracks.item_chance = {
+  outside = Building.item_chance.outside,
+  inside = {
+    Newspaper = 1.00,
+  }
+}
+
+-------------------------------------------------------------------
+
+return {Gatehouse, Infirmary, Armory, Supply, Barracks}
