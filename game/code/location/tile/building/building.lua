8267ab06611cc55b9b6edc4122d61aedb67d9094
Timothy <timothymtorres@gmail.com>
2018-10-20 22:03:17 -0500

    Add table to Building to hold machine instances
    Couple of changes:
    - Moved around a lot of the old references to equipment.  Instead of
    self.generator, it's now self.equipment.generator
    - Deprecated some settings for isPresent method
    - Removed need to make machines lowercase (handled in machine modules)

--- game/code/location/tile/building/building.lua
+++ game/code/location/tile/building/building.lua
@@ -68,7 +68,9 @@ function Building:initialize(map, y, x, name)
   
   self.door        = Door:new(self)
   self.barricade   = Barricade:new(self)
-  self.integrity   = Integrity:new(self) 
+  self.integrity   = Integrity:new(self)
+
+  self.equipment   = {}
 end
 
 function Building:insert(player, setting) 
@@ -84,59 +86,47 @@ function Building:remove(player, setting)
 end
 
 function Building:install(machine, condition) 
-  local Machine = machine:sub(1,1):upper()..machine:sub(2)  -- capitalize machine
-  self[machine] = Machines[Machine]:new(self, condition) 
+  self.equipment[machine] = Machines[machine]:new(self, condition)
 end
 
 function Building:blackout()  -- change method name to setPower?
   -- 3x3 area do blackout event on tile(s)
 end
 
-function Building:getBarrier() return (self.barricade:getHP() > 0 and 'barricade') or (self.door:getHP() > 0 and 'door') end  -- should this be a class?
+function Building:getBarrier() return (self.barricade:getHP() > 0 and 'barricade') or (self.door:getHP() > 0 and 'door') end  -- should this return a class?
 
-function Building:getEquipment()
-  local machines = {}
-  for Machine in ipairs(Machines) do
-    machine = string.lower(tostring(Machine))
-    if self[machine] then machines[#machines+1] = self[machine] end
-  end
-  return machines
-end
+function Building:getEquipment() return self.equipment end
 
-function Building:getMachine(machine) return self[machine] end
+function Building:getMachine(machine) return self.equipment[machine] end
 
 -- function Building:getPos() return (NO NEED?)
 
 function Building:isPresent(setting)
   if setting == 'machines' then
-    for Machine in ipairs(Machines) do
-      machine = string.lower(tostring(Machine))
-      if self[machine] then return true end
-    end
-    return false
-  elseif setting == 'powered machines' then 
-    return self:isPresent('machines') and self:isPowered()
-  elseif setting == 'damaged machines' then
-    for Machine in ipairs(Machines) do
-      machine = string.lower(tostring(Machine))
-      if self[machine] and self[machine]:isDamaged() then return true end
-    end
-    return false
+    if next(self.equipment) then return true else return false end    
   elseif setting == 'door' then
     return self.door and true or false
   elseif setting == 'damaged door' then
     return (self.door and self.door:isDamaged()) or false
   else -- individual machine
     local machine = setting
-    return self[machine]
+    return self.equipment[machine] and true or false
   end
+--[[
+  Pretty sure these settings are redundant, but I will hang on to them just in case...
+
+  elseif setting == 'powered machines' then return self:isPresent('machines') and self:isPowered()
+  elseif setting == 'damaged machines' then
+    for _, machine in pairs(self.equipment) do if machine:isDamaged() then return true end end
+    return false  
+--]]
 end
 
 -- function Building:isOccupied(mob_type)  great way to check for people inside when ruining
 
 function Building:isFortified() return self.barricade:getHP() > 0 and self.door:getHP() > 0 end
 
-function Building:isPowered() return self.generator and self.generator:isActive() end
+function Building:isPowered() return self.equipment.generator and self.equipment.generator:isActive() end
 
 function Building:isOpen() return self.barricade:isDestroyed() and (self.door:isOpen() or self.door:isDestroyed()) end
 
