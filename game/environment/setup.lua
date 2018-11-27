c3bf1da051070d9d6bf0eab52542b198c9c44486
Timothy <timothymtorres@gmail.com>
2018-11-26 22:17:08 -0600

    Add comment explaining how to use environment/setup.lua

--- game/environment/setup.lua
+++ game/environment/setup.lua
@@ -1,3 +1,6 @@
+-- this sets up objects and events in the game for testing 
+-- here you can load different things: 
+-- a generator full of fuel, a hurt character, different items, performing actions with players, etc.
 local setup = function(...)
 	for _, environment in ipairs({...}) do
 		require('environment.'..environment)
