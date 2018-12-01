a776ce453ec7bfb7a763d056360eb7722b229116
Timothy <timothymtorres@gmail.com>
2018-11-30 18:34:06 -0600

    Fix search_area tap events behaving strange when moved
    The tap event for search_area was moving the map when moved off the
    icon.  This is due to the tap event triggering the map unintentionally
    because the tap event triggered down beneath layers.

--- game/scenes/objects/search_area.lua
+++ game/scenes/objects/search_area.lua
@@ -9,16 +9,19 @@ local composer = require( "composer" )
 local searchTimer
 
 local function searchTrigger(event)
+	print(table.inspect(searchTimer))
 	print('YOU HAVE SEARCHED')
 end
 
 local function touchListener(event)
-print('TouchListener for search_area has been triggered')
     if ( event.phase == "began" ) then
         display.getCurrentStage():setFocus( event.target )  --'event.target' is the touched object
         searchTimer = timer.performWithDelay(300, searchTrigger, 0)
         return true
-    elseif ( event.phase == "moved" or event.phase == "ended" or event.phase == "cancelled" ) then
+    elseif (event.phase == "moved") then
+    	timer.cancel(searchTimer)
+    	return true
+    elseif ( event.phase == "ended" or event.phase == "cancelled" ) then
         display.getCurrentStage():setFocus( nil )  --setting focus to 'nil' removes the focus
         timer.cancel(searchTimer)
         return true
@@ -28,23 +31,21 @@ end
 
 local function tapListener(event)
     if ( event.numTaps == 2 ) then
-        print( "object double-tapped = "..tostring(event.target) )
-        print('You have searched '..(event.numTaps - 1).. ' times')
-    else
-        return true
+    	searchTrigger(event)
     end
 end
 
 function M.new( object )	
 	if not object then error( "ERROR: Expected display visual" ) end
 	local search_area = object:getVisual() 
+	search_area.alpha = 0.01 -- the minimal alpha for tap/touch events to register
+
 	local player_stage = main_player:getStage()
 	local search_area_is_same_stage_as_player = object.name == player_stage
 
 	if search_area_is_same_stage_as_player and main_player:isMobType('human') then
-		search_area:addEventListener("tap", tapListener)
-		search_area:addEventListener("touch", touchListener)	
-
+		search_area:addEventListener("touch", touchListener)
+		search_area:addEventListener("tap", tapListener)		
 	end
 
 	return search_area
