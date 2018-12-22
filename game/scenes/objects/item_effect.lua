12f0887bf83f929f6c842de073e6856671fa0270
Timothy <timothymtorres@gmail.com>
2018-12-21 20:06:30 -0600

    Add item_effect object to game

--- game/scenes/objects/item_effect.lua
+++ game/scenes/objects/item_effect.lua
@@ -6,20 +6,11 @@ local M = {}
 local itemSheetInfo = require('graphics.ss13.items.items')
 local Object = require('code.libs.berry.Object')
 
-function M.new(object, map, item, x, y)
---[[
-	local Effects_layer = map:getObjectLayer('Effects')
-
-print(table.inspect(Effects_layer, {depth=2}))
-
-    local item_effect = Effects_layer:getObject('meowth')
-	transition.to( item_effect.sprite, { time=1, x=x, y=y, iterations = 1} )
---]]
-
+function M.new(map, item, x, y)
     local fake_json_data = {
       image = 'graphics/ss13/items/items.png',
       imageSheetInfo = itemSheetInfo,
-      name = string.lower(tostring(item)),
+      name = 'background',
       height = 32,
       rotation = 0,  -- you may need to subtract map.world.rotation from rotation
       type = "item_effect",
@@ -31,47 +22,19 @@ print(table.inspect(Effects_layer, {depth=2}))
 
 	local Effects_layer = map:getObjectLayer('Effects')
 
-    local item_effect = Object:new(fake_json_data, map, Effects_layer)
-    Effects_layer:addObject(item_effect)
-    item_effect:create()
+    local item_effect_bkgr_obj = Object:new(fake_json_data, map, Effects_layer)
+    Effects_layer:addObject(item_effect_bkgr_obj)
+    item_effect_bkgr_obj:create()
 
-print('---------------------')
-print('item_effect vars')
-print('---------------------')
-for k,v in pairs(item_effect) do print(k,v) end
-print('---------------------')
-for k,v in pairs(item_effect.sprite) do print(k,v) end
+    fake_json_data.name = string.lower(tostring(item))
+    local item_effect_obj = Object:new(fake_json_data, map, Effects_layer)
+    Effects_layer:addObject(item_effect_obj)
+    item_effect_obj:create()
 
-	transition.to( item_effect, { time=1, x=x, y=y, iterations = 1} )
+    local item_effect = item_effect_obj:getVisual()
+    item_effect.bkgr = item_effect_bkgr_obj:getVisual()
 
-
-
-
-	--[[
-	-- Create display group to hold visuals
-	local group = display.newGroup()
-
-	local itemSheet = graphics.newImageSheet('graphics/ss13/items/items.png', itemSheetInfo:getSheet())
-
-	item = display.newImage(itemSheet, itemSheetInfo:getFrameIndex('crowbar'))
-	item.x = x - 100
-	item.y = y
-
-	item_bkgr = display.newImage(itemSheet, itemSheetInfo:getFrameIndex('background'))
-	item_bkgr.x = x - 100
-	item_bkgr.y = y
-
-	group:insert(item_bkgr)
-	group:insert(item)
-
-	function group:finalize()
-		-- On remove, cleanup instance 
-	end
-	group:addEventListener( "finalize" )
-
-	-- Return instance
-	return group
-	--]]
+	return item_effect
 end
 
 return M
