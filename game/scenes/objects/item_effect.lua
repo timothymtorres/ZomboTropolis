d9f84f397cfcc6b9874b5da6f48dfb2fe84cf424
Timothy <timothymtorres@gmail.com>
2018-12-22 00:34:34 -0600

    Change item_effect scene object to return object instead of sprite
    Made some small directory fixes too since we changed the image and
    spritesheet locations.

--- game/scenes/objects/item_effect.lua
+++ game/scenes/objects/item_effect.lua
@@ -3,12 +3,12 @@
 
 -- Define module
 local M = {}
-local itemSheetInfo = require('graphics.ss13.items.items')
+local itemSheetInfo = require('graphics.ss13.items')
 local Object = require('code.libs.berry.Object')
 
 function M.new(map, item, x, y)
     local fake_json_data = {
-      image = 'graphics/ss13/items/items.png',
+      image = 'graphics/ss13/items.png',
       imageSheetInfo = itemSheetInfo,
       name = 'background',
       height = 32,
@@ -22,17 +22,17 @@ function M.new(map, item, x, y)
 
 	local Effects_layer = map:getObjectLayer('Effects')
 
-    local item_effect_bkgr_obj = Object:new(fake_json_data, map, Effects_layer)
-    Effects_layer:addObject(item_effect_bkgr_obj)
-    item_effect_bkgr_obj:create()
+    local item_effect_bkgr = Object:new(fake_json_data, map, Effects_layer)
+    Effects_layer:addObject(item_effect_bkgr)
+    item_effect_bkgr:create()
 
     fake_json_data.name = string.lower(tostring(item))
-    local item_effect_obj = Object:new(fake_json_data, map, Effects_layer)
-    Effects_layer:addObject(item_effect_obj)
-    item_effect_obj:create()
+    local item_effect = Object:new(fake_json_data, map, Effects_layer)
+    Effects_layer:addObject(item_effect)
+    item_effect:create()
 
-    local item_effect = item_effect_obj:getVisual()
-    item_effect.bkgr = item_effect_bkgr_obj:getVisual()
+    -- attach our background objec to item_effect
+    item_effect.bkgr = item_effect_bkgr
 
 	return item_effect
 end
