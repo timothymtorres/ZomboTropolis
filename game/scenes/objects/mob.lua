e15e440f039116f1d2c51ac8e41be57c0d8ea3c6
Tim <timothymtorres@gmail.com>
2019-04-08 23:46:50 -0500

    Add fixed rotation to mob sprites

--- game/scenes/objects/mob.lua
+++ game/scenes/objects/mob.lua
@@ -1,11 +1,13 @@
 local function Plugin(mob)	
 	if not mob then error( "ERROR: Expected display visual" ) end
 
-    mob:pause()
-    mob:setFrame(math.random(4))
-    local physics_properties = {bounce = 1}
-    physics.addBody(mob, physics_properties )
- 
+	mob:pause()
+	mob:setFrame(math.random(4))
+	local physics_properties = {bounce = 1}
+	physics.addBody(mob, physics_properties )
+  mob.isFixedRotation = true
+	mob:setLinearVelocity(32, 0)
+
 	return mob
 end
 
