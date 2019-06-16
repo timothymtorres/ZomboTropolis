eea666b1d2bafcb6a6094b425805c6724afbbd3b
Tim <timothymtorres@gmail.com>
2019-06-16 18:40:42 -0500

    Add default tileset template for mob images

---
+++ game/graphics/mob/default_tileset.lua
@@ -0,0 +1,204 @@
+return {
+  version = "1.2",
+  luaversion = "5.1",
+  tiledversion = "1.2.3",
+  name = "darkelf",
+  tilewidth = 64,
+  tileheight = 64,
+  spacing = 0,
+  margin = 0,
+  columns = 13,
+  image = "darkelf.png",
+  imagewidth = 832,
+  imageheight = 1344,
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 64,
+    height = 64
+  },
+  properties = {},
+  terrains = {},
+  tilecount = 273,
+  tiles = {
+    {
+      id = 104,
+      properties = {
+        ["name"] = "walk-north"
+      },
+      animation = {
+        {
+          tileid = 104,
+          duration = 100
+        },
+        {
+          tileid = 105,
+          duration = 100
+        },
+        {
+          tileid = 106,
+          duration = 100
+        },
+        {
+          tileid = 107,
+          duration = 100
+        },
+        {
+          tileid = 108,
+          duration = 100
+        },
+        {
+          tileid = 109,
+          duration = 100
+        },
+        {
+          tileid = 110,
+          duration = 100
+        },
+        {
+          tileid = 111,
+          duration = 100
+        },
+        {
+          tileid = 112,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 117,
+      properties = {
+        ["name"] = "walk-west"
+      },
+      animation = {
+        {
+          tileid = 117,
+          duration = 100
+        },
+        {
+          tileid = 118,
+          duration = 100
+        },
+        {
+          tileid = 119,
+          duration = 100
+        },
+        {
+          tileid = 120,
+          duration = 100
+        },
+        {
+          tileid = 121,
+          duration = 100
+        },
+        {
+          tileid = 122,
+          duration = 100
+        },
+        {
+          tileid = 123,
+          duration = 100
+        },
+        {
+          tileid = 124,
+          duration = 100
+        },
+        {
+          tileid = 125,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 130,
+      properties = {
+        ["name"] = "walk-south"
+      },
+      animation = {
+        {
+          tileid = 130,
+          duration = 100
+        },
+        {
+          tileid = 131,
+          duration = 100
+        },
+        {
+          tileid = 132,
+          duration = 100
+        },
+        {
+          tileid = 133,
+          duration = 100
+        },
+        {
+          tileid = 134,
+          duration = 100
+        },
+        {
+          tileid = 135,
+          duration = 100
+        },
+        {
+          tileid = 136,
+          duration = 100
+        },
+        {
+          tileid = 137,
+          duration = 100
+        },
+        {
+          tileid = 138,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 143,
+      properties = {
+        ["name"] = "walk-east"
+      },
+      animation = {
+        {
+          tileid = 143,
+          duration = 100
+        },
+        {
+          tileid = 144,
+          duration = 100
+        },
+        {
+          tileid = 145,
+          duration = 100
+        },
+        {
+          tileid = 146,
+          duration = 100
+        },
+        {
+          tileid = 147,
+          duration = 100
+        },
+        {
+          tileid = 148,
+          duration = 100
+        },
+        {
+          tileid = 149,
+          duration = 100
+        },
+        {
+          tileid = 150,
+          duration = 100
+        },
+        {
+          tileid = 151,
+          duration = 100
+        }
+      }
+    }
+  }
+}
---
+++ game/graphics/mob/default_tileset.tsx
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="darkelf" tilewidth="64" tileheight="64" tilecount="273" columns="13">
+ <image source="darkelf.png" width="832" height="1344"/>
+ <tile id="104">
+  <properties>
+   <property name="name" value="walk-north"/>
+  </properties>
+  <animation>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="106" duration="100"/>
+   <frame tileid="107" duration="100"/>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="109" duration="100"/>
+   <frame tileid="110" duration="100"/>
+   <frame tileid="111" duration="100"/>
+   <frame tileid="112" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="name" value="walk-west"/>
+  </properties>
+  <animation>
+   <frame tileid="117" duration="100"/>
+   <frame tileid="118" duration="100"/>
+   <frame tileid="119" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="121" duration="100"/>
+   <frame tileid="122" duration="100"/>
+   <frame tileid="123" duration="100"/>
+   <frame tileid="124" duration="100"/>
+   <frame tileid="125" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="name" value="walk-south"/>
+  </properties>
+  <animation>
+   <frame tileid="130" duration="100"/>
+   <frame tileid="131" duration="100"/>
+   <frame tileid="132" duration="100"/>
+   <frame tileid="133" duration="100"/>
+   <frame tileid="134" duration="100"/>
+   <frame tileid="135" duration="100"/>
+   <frame tileid="136" duration="100"/>
+   <frame tileid="137" duration="100"/>
+   <frame tileid="138" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="name" value="walk-east"/>
+  </properties>
+  <animation>
+   <frame tileid="143" duration="100"/>
+   <frame tileid="144" duration="100"/>
+   <frame tileid="145" duration="100"/>
+   <frame tileid="146" duration="100"/>
+   <frame tileid="147" duration="100"/>
+   <frame tileid="148" duration="100"/>
+   <frame tileid="149" duration="100"/>
+   <frame tileid="150" duration="100"/>
+   <frame tileid="151" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
