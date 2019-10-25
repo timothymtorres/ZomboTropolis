c87e9dc0ddc0c58e634aa80ef8d32d66186054b9
Tim <timothymtorres@gmail.com>
2019-10-25 02:31:55 -0500

    Fix map minimum zoom out boundary

--- game/scenes/map.lua
+++ game/scenes/map.lua
@@ -60,7 +60,7 @@ end
 
 -- The zooming in/out is still broken as fuck!
 local scale = world.xScale -- both xScale & yScale should be same
-local MAX_SCALE, MIN_SCALE = 2, -55 --1
+local MAX_SCALE, MIN_SCALE = 2, 1
 local lastEvent = {}
 
 local function key( event )
