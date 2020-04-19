86f202a4b42a5e0f0e53689c9e6c5511ec7c3f39
Tim <timothymtorres@gmail.com>
2020-04-19 13:24:59 -0500

    Fix actions table overwrite action for both mob types
    Actions that are used by both mob types were being overwritten.  (move, enter, exit, etc.) and this was bad.  Therefore removed the global action table and just gave both mob types their own seperate action tables.  No more combined table.

--- game/code/player/actions.lua
+++ game/code/player/actions.lua
@@ -18,31 +18,25 @@ local actions_CSV = csv_helpers.convertToLua(actions_path)
 for _, action in ipairs(actions_CSV) do
 	local mob_type = action.MOB_TYPE
 
-	-- copy action data to mob_type table
-	-- we can use ipairs to get a list of actions 
-	actions[mob_type][#actions[mob_type]] = action
+	actions[mob_type][action.ID] = action
 	actions[mob_type][action.NAME] = action
-
-	-- copy ALL actions data to table
-	-- the action.ID is used for the server/client communication
-	actions[action.ID] = action
-	actions[action.NAME] = action
-
 	-- what about abilities/equipment/items
-	-- we need to import action activate/client & server critera to this
 end
 
 -- this combines the action activate/critera functions with the CSV data into one table
-local addFunctionsToActions = function(...)
+local addFunctionsToActions = function(mob_type, ...)
 	for _, actions_table in ipairs({...}) do
 		for name, functions in pairs(actions_table) do 
-			actions[name] = lume.merge(actions[name], functions) 
+			local action = lume.merge(actions[mob_type][name], functions) -- merge functions & CSV data
+
+			actions[mob_type][name] = action
+			actions[mob_type][action.ID] = action 
 		end
 	end
 end
 
 -- import activate/server_critera/client_critera functions to actions
-addFunctionsToActions(human_advanced_actions, human_basic_actions) -- for humans
-addFunctionsToActions(zombie_advanced_actions, zombie_basic_actions) -- for zombies
+addFunctionsToActions('human', human_advanced_actions, human_basic_actions)
+addFunctionsToActions('zombie', zombie_advanced_actions, zombie_basic_actions) 
 
 return actions
