ac7297029376b7fe249723000b9bb7a35055fb5d
Tim <timothymtorres@gmail.com>
2020-04-22 17:03:45 -0500

    Add durability updates and destruction for machines

--- game/code/location/tile/building/machine/machine.lua
+++ game/code/location/tile/building/machine/machine.lua
@@ -1,4 +1,5 @@
 local class = require('code.libs.middleclass')
+string.replace = require('code.libs.replace')
 
 local Machine = class('Machine')
 local MAX_HP = 8
@@ -11,13 +12,42 @@ end
 
 function Machine:destroy()
   local building = self.building
-  building.equipment[tostring(self)] = nil
-  -- update building stuff... (ie. power out if generator, player notification event, broadcast event, etc.)
+  local machine = tostring(self)
+  building.equipment[machine] = nil
+
+  -- update building stuff... (ie. power out if generator)
+
+  --------------------------------------------
+  -----------   M E S S A G E   --------------
+  --------------------------------------------
+  
+  local public_msg = 'The {machine} is destroyed!'
+  
+  local names = {machine=machine}
+  public_msg = public_msg:replace(names)
+    
+  --------------------------------------------
+  ---------   B R O A D C A S T   ------------
+  --------------------------------------------
+
+  local event = {'machine', message}
+  local settings = {stage='inside'}
+  
+  building:broadcastEvent(public_msg, event, settings) 
 end
 
 function Machine:updateHP(num)
   self.hp = math.min(math.max(self.hp + num, 0), max_hp) 
   if self.hp == 0 then self:destroy() end  
+end
+
+function Machine:updateDurability()
+  local failed_durability_test = math.random(self.DURABILITY) <= 1
+
+  if failed_durability_test then 
+    local condition = item:updateCondition(-1) 
+    if condition == 0 then self:destroy() end
+  end
 end
 
 function Machine:updateCondition(num)
